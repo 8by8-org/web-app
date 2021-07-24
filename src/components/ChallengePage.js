76ab6f4c6dc240e892cb0c21ce05429143c3d14b
kphan20 <knpmths@gmail.com>
2021-07-24 14:25:21 -0400

    Basic implementation of challenge page invite people popup

--- src/components/ChallengePage.js
+++ src/components/ChallengePage.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useHistory } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../contexts/AuthContext';
+import ChallengePagePopup from './ChallengePagePopup';
 
 export default function ChallengePage() {
     const { currentUser } = useAuth();
+    const [popupVisible, togglePopupVisible] = useState(false);
+    const togglePopup = () => {
+        togglePopupVisible(!popupVisible);
+    }
     const history = useHistory();
     return (
         <div>
             <p>current login status (debug purposes): {currentUser === null ? 'Logged out' : currentUser.email}</p>
             <p>I've changed this!</p>
             <p>Challenge page</p>
+            {!popupVisible && <button onClick={togglePopup}>Invite friends</button>}
+            {popupVisible && <ChallengePagePopup togglePopup={togglePopup}/>}
             <button onClick={() => history.push('/logout')}>Logout</button>
         </div>
     )
