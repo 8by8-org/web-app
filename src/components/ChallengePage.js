a7833a39082dfade152ac0d1d7ad214c259a051a
Simon Cheng <simoncheng559@gmail.com>
2021-07-25 21:51:37 -0400

    prevent button from disappearing (we will be greying out the background when the popup shows anyway)

--- src/components/ChallengePage.js
+++ src/components/ChallengePage.js
@@ -15,7 +15,7 @@ export default function ChallengePage() {
             <p>current login status (debug purposes): {currentUser === null ? 'Logged out' : currentUser.email}</p>
             <p>I've changed this!</p>
             <p>Challenge page</p>
-            {!popupVisible && <button onClick={togglePopup}>Invite friends</button>}
+            {<button onClick={togglePopup}>Invite friends</button>}
             {popupVisible && <ChallengePagePopup togglePopup={togglePopup}/>}
             <button onClick={() => history.push('/logout')}>Logout</button>
         </div>
