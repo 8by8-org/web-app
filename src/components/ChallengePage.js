91a50a71b34b9ac24c05010d35f08cf1a7c109dd
AnaB <ana.batranovic@gmail.com>
2021-07-30 17:22:33 +0200

    added icon component which consists of 16 switchable svgs (8 for uncompleted and 8 for completed) for the challenger page

--- src/components/ChallengePage.js
+++ src/components/ChallengePage.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import ChallengePagePopup from './ChallengePagePopup';
+import SVGIcon from "./IconComponent";
 
 export default function ChallengePage() {
     const { currentUser } = useAuth();
@@ -15,6 +16,16 @@ export default function ChallengePage() {
             <p>current login status (debug purposes): {currentUser === null ? 'Logged out' : currentUser.email}</p>
             <p>I've changed this!</p>
             <p>Challenge page</p>
+            <SVGIcon name="icon1uncompleted" width={100} />
+            <SVGIcon name="icon2uncompleted" width={100} />
+            <SVGIcon name="icon3uncompleted" width={100} />
+            <SVGIcon name="icon4uncompleted" width={100} />
+            <br/>
+            <SVGIcon name="icon5uncompleted" width={100} />
+            <SVGIcon name="icon6uncompleted" width={100} />
+            <SVGIcon name="icon7uncompleted" width={100} />
+            <SVGIcon name="icon8uncompleted" width={100} />
+            <br/>
             {<button onClick={togglePopup}>Invite friends</button>}
             {popupVisible && <ChallengePagePopup togglePopup={togglePopup}/>}
             <button onClick={() => history.push('/logout')}>Logout</button>
