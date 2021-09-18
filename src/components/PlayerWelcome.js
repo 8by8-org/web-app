cbf0f95ecb25d58b858c189e6fad6ea0298154dd
Arnab Ghosh <arnabcare21@gmail.com>
2021-09-18 11:46:51 -0700

    Make the query param a base64 string

--- src/components/PlayerWelcome.js
+++ src/components/PlayerWelcome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LandingPageInfo from "./LandingPageInfo";
 import { doc, getDoc, getFirestore } from "@firebase/firestore";
 
+
 class PlayerWelcome extends React.Component{
     // Only these 3 functions are needed
     // Render is only to show that the data is rendered
@@ -12,13 +13,19 @@ class PlayerWelcome extends React.Component{
             users: null
         }
     }
+    generateURL = (uid, params /* Put the params you want here */) => (
+        `${window.location.origin}/playerwelcome?uid=${btoa(`${uid},${params.join(',')}`)}`
+    )
     useQuery = () => new URLSearchParams(this.props.location.search);
     componentDidMount() {
         this.renderUsers();
     }
     async GetUserData() {
         try {
-            const uid = this.useQuery().get('userId');
+            // See the log for what data is stored in the URL
+            // We can decide and change what this is at any time
+            console.log(atob(this.useQuery().get('uid')).split(','));
+            const uid = atob(this.useQuery().get('uid')).split(',')[0];
             const db = getFirestore();
             const docRef = doc(db, 'users', uid);
             const docSnap = await getDoc(docRef);
@@ -50,6 +57,7 @@ class PlayerWelcome extends React.Component{
       
     render() {
         const users = this.state.users;
+        const url = this.generateURL('iLjxSYwS3xNlZh3dZuxMrKX3IKo2', ['9/18/2021','John Doe'])
         return (
             
             <div>
@@ -57,7 +65,11 @@ class PlayerWelcome extends React.Component{
                 <ul className="list-group list-group-flush">
                     {users}
                 </ul>
+                <p>
+                    {url}
+                </p>
             </div>
+            
         );
     }
 }
