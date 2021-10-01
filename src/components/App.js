32d3274ff141404a3e94b310c96347c0201302ae
Arnab Ghosh <arnabcare21@gmail.com>
2021-10-01 09:21:57 -0700

    fix some conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,49 @@
-import React from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import { AuthProvider } from '../contexts/AuthContext';
-import ChallengePage from './ChallengePage';
-import Login from './LoginPage';
-import Logout from './Logout';
-import PrivateRoute from './PrivateRoute';
-import Header from './Header';
-import './App.css';
-import HomePage from './HomePage'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
+import ChallengePage from "./ChallengePage";
+import Login from "./LoginPage";
+import Logout from "./Logout";
+import PrivateRoute from "./PrivateRoute";
+import Header from "./Header";
+import "./App.css";
+import HomePage from "./HomePage";
+import ChallengerWelcome from "./ChallengerWelcome";
 
 function App() {
-    return (
-        <>
-            <AuthProvider>
-                <Header />
-                <div id='app-outer-wrapper'>
-                    <div id='app-wrapper'>
-                        <Router>
-                            <Switch>
-                            <Route exact path="/"><Redirect to="/homepage" /></Route>
-                                <Route path='/homepage' component={HomePage} />
-                                <Route path='/login' component={Login} />
-                                <PrivateRoute path='/challenge' component={ChallengePage} />
-                                <PrivateRoute path='/logout' component={Logout} />
-                            </Switch>
-                        </Router>
-                    </div>
-                </div>
-            </AuthProvider>
-        </>
-    );
+  return (
+    <>
+      <AuthProvider>
+        <div id="header">
+          <Header />
+        </div>
+        <div id="app-outer-wrapper">
+          <div id="app-wrapper">
+            <Router>
+              <Switch>
+                <Route exact path="/">
+                  <Redirect to="/homepage" />
+                </Route>
+                <Route path="/homepage" component={HomePage} />
+                <Route
+                  path="/challengerwelcome"
+                  component={ChallengerWelcome}
+                />
+                <Route path="/login" component={Login} />
+                <PrivateRoute path="/challenge" component={ChallengePage} />
+                <PrivateRoute path="/logout" component={Logout} />
+              </Switch>
+            </Router>
+          </div>
+        </div>
+      </AuthProvider>
+    </>
+  );
 }
 
 export default App;
