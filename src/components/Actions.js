316b8b7dd6214fbb14ed1aa2f2a787c077152da1
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-11-08 17:30:39 -0500

    updated styles, added startChallenge function, removed uneeded hooks
    Signed-off-by: xJamesScott <50638563+xJamesScott@users.noreply.github.com>

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,36 +1,80 @@
-import React from 'react'
-import { Row, Button, Container } from 'react-bootstrap'
-import { useHistory } from 'react-router'
-import './Actions.css'
+import React from 'react';
+import { Row, Button, Container } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import { useAuth } from "../contexts/AuthContext";
+import { db } from "../firebase";
+import Avatar from "../assets/avatars/Girl-2.png";
+import './Actions.scss';
 
 export default function Actions() {
+    const actionDivStyle = {
+        padding: "0px 10px 10px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+    };
+
+
+    const { currentUser: { uid: challengerId, name } } = useAuth();
     const history = useHistory();
+    
+    // creates challenge data structure
+    const startChallenge = () => {
+        const now = new Date;
+        const challengeEndStamp = new Date(now.setDate(now.getDate() + 8));
+        db.collection("users").doc(challengerId).collection("challenge").doc("challenge")
+            .set({
+                badges: [],
+                challengeEnd: challengeEndStamp
+            });
+        
+        history.push('/challengerwelcome')
+    };
+
     return (
         <div>
-            <Container fluid className="py-4 px-5">
-                <Row align="center">
-                    <h1>Take Action</h1>
-                </Row>
-                <Row align="center">
-                    <p>Your actions matter towards better AAPI representation in our country.</p>
-                </Row>
-                <Row>
-                    <p>user icon</p>
-                </Row>
-                <Row>
-                    <p>You're taking action for:</p>
-                    <p>(Get challenger info from url)</p>
-                </Row>
-                <Row className="py-2">
-                    <Button className="py-2" id="button-style" onClick={() => {history.push('/voterreg'); localStorage.setItem('player', 'voter')}}>Register to vote</Button>
-                </Row>
-                <Row className="py-2">
-                    <Button className="py-2" id="button-style" onClick={() => {localStorage.setItem('player', 'reminder')}}>Get election reminders</Button>
-                </Row>
-                <Row className="py-2">
-                    <Button className="py-2" id="button-style" onClick={() => {history.push('/challengerwelcome')}}>Take the challenge yourself</Button>
-                </Row>
-            </Container>
+            <div style={actionDivStyle} className="action-page">
+                <h1 className="bebas-neue take-action">TAKE ACTION</h1>
+                <p className="lato sub-title">Thanks for registering to vote! Henry will get one badge
+                    because of your action. Well done!
+                </p>
+                
+                {/* avatar */}
+                <img className="actions avatar-image" src={Avatar} alt="Avatar" />
+
+                <p className="lato tiny-text">You're taking action for:</p>
+                
+                {/* player name */}
+                <h2 className="lato challenge-name">{name ? name : "Player"}'s Challenge</h2>
+               
+               
+                {/* yellow buttons */}
+                <div className="d-flex justify-content-center buttons-container">
+                    <button className="avatar-button">
+                        <p className="lato avatar-button-text">Send Emoji</p>
+                    </button>
+                    <button className="avatar-button">
+                        <p className="lato avatar-button-text">Share</p>
+                    </button>
+                </div>
+                <p className="lato cta-pre-text">You can still help the AAPI community by
+                    taking another action!</p>
+                
+                {/* grey buttons */}
+                <div>
+                    <button className="gray-button">
+                        <p className="lato gray-button-text">Get election reminders</p>
+                    </button>
+                    <button className="gray-button second" onClick={startChallenge}>
+                        <p className="lato gray-button-text">Take the challenge yourself</p>
+                    </button>
+                </div>
+
+                <p className="lato tiny-text user-question">Looking for something else?</p>
+                <p className="lato tiny-text link-text restart">
+                    <a className="lato" href="http://www.google.com">Restart voter registration</a>
+                </p>
+            </div>
         </div>
-    )
-}
+    );
+};
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
-import 'firebase/auth'
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const app = firebase.initializeApp({
     apiKey: "AIzaSyDhMPNp_v1vl1APcbIuwhEYVwm0DJSckZg",
@@ -11,4 +12,6 @@ const app = firebase.initializeApp({
 });
 
 export const auth = app.auth();
+export const db = app.firestore();
+
 export default app;
