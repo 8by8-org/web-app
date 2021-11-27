2f52e96b0c2bec5d6d40b2ee016dd758b193f4a1
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-11-27 10:17:28 -0500

    updated firestore actions to V9

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,91 +1,102 @@
-import React, { useState } from 'react';
-import { useHistory, useParams, matchPath } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams, matchPath } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
 import { db } from "../firebase";
+import { getDoc, setDoc, doc } from "firebase/firestore";
 import Avatar from "../assets/avatars/Girl-2.png";
-import './Actions.scss';
+import "./Actions.scss";
+import { GiConsoleController } from "react-icons/gi";
 
 export default function Actions() {
-    const actionDivStyle = {
-        padding: "0px 10px 10px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-    };
+  const actionDivStyle = {
+    padding: "0px 10px 10px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  };
 
-    const { id: challengerId } = useParams(); // from url parameter
-    const [challengerName, setChallengerName] = useState(null);
-    const { currentUser: { uid: playerId } } = useAuth();
+  const { id: challengerId } = useParams(); // from url parameter
+  const [challengerName, setChallengerName] = useState(null);
+  const {
+    currentUser: { uid: playerId },
+  } = useAuth();
 
-    // fetches name from database with challengerId
-    db.collection("users").doc(challengerId)
-        .get()
-        .then((doc) => {
-            const name = doc.data().name
-            if (name) {
-                setChallengerName(name)
-            }
-        });
+  useEffect(() => {
+    getDoc(doc(db, "users", challengerId)).then((docSnap) => {
+      if (docSnap.exists()) {
+        setChallengerName(docSnap.data().name);
+      } else {
+        console.log("No such document!");
+      }
+    });
+  }, [challengerId]);
 
-    const history = useHistory();
+  //   users()
 
-    // creates challenge data structure
-    const startChallenge = () => {
-        const now = new Date;
-        const challengeEndStamp = new Date(now.setDate(now.getDate() + 8));
-        db.collection("users").doc(challengerId).collection("challenge").doc("challenge")
-            .set({
-                badges: [],
-                challengeEnd: challengeEndStamp
-            });
+  const history = useHistory();
 
-        history.push('/challengerwelcome')
-    };
+  // creates challenge data structure
+  const startChallenge = () => {
+    const now = new Date();
+    const challengeEndStamp = new Date(now.setDate(now.getDate() + 8));
+     setDoc(doc(db, "users", challengerId, "challenge", "challenge"), {
+      badges: [],
+      challengeEnd: challengeEndStamp,
+    });
+    history.push("/challengerwelcome");
+  };
 
-    return (
-        <div>
-            <div style={actionDivStyle} className="action-page">
-                <h1 className="bebas-neue take-action">TAKE ACTION</h1>
-                <p className="lato sub-title">Thanks for registering to vote! Henry will get one badge
-                    because of your action. Well done!
-                </p>
+  return (
+    <div>
+      <div style={actionDivStyle} className="action-page">
+        <h1 className="bebas-neue take-action">TAKE ACTION</h1>
+        <p className="lato sub-title">
+          Thanks for registering to vote! Henry will get one badge because of
+          your action. Well done!
+        </p>
 
-                {/* avatar */}
-                <img className="actions avatar-image" src={Avatar} alt="Avatar" />
+        {/* avatar */}
+        <img className="actions avatar-image" src={Avatar} alt="Avatar" />
 
-                <p className="lato tiny-text">You're taking action for:</p>
+        <p className="lato tiny-text">You're taking action for:</p>
 
-                {/* player name */}
-                <h2 className="lato challenge-name">{challengerName ? challengerName : "Player"}'s Challenge</h2>
+        {/* player name */}
+        <h2 className="lato challenge-name">
+          {challengerName ? challengerName : "Player"}'s Challenge
+        </h2>
 
-
-                {/* yellow buttons */}
-                <div className="d-flex justify-content-center buttons-container">
-                    <button className="avatar-button">
-                        <p className="lato avatar-button-text">Send Emoji</p>
-                    </button>
-                    <button className="avatar-button">
-                        <p className="lato avatar-button-text">Share</p>
-                    </button>
-                </div>
-                <p className="lato cta-pre-text">You can still help the AAPI community by
-                    taking another action!</p>
-
-                {/* action buttons */}
-                <div className="action-buttons-container">
-                    <button className="gray-button">
-                        <p className="lato gray-button-text">Get election reminders</p>
-                    </button>
-                    <button className="gray-button" onClick={startChallenge}>
-                        <p className="lato gray-button-text">Take the challenge yourself</p>
-                    </button>
-                </div>
-
-                <p className="lato tiny-text user-question">Looking for something else?</p>
-                <p className="lato tiny-text link-text restart">
-                    <a className="lato" href="http://www.google.com">Restart voter registration</a>
-                </p>
-            </div>
+        {/* yellow buttons */}
+        <div className="d-flex justify-content-center buttons-container">
+          <button className="avatar-button">
+            <p className="lato avatar-button-text">Send Emoji</p>
+          </button>
+          <button className="avatar-button">
+            <p className="lato avatar-button-text">Share</p>
+          </button>
         </div>
-    );
-};
+        <p className="lato cta-pre-text">
+          You can still help the AAPI community by taking another action!
+        </p>
+
+        {/* action buttons */}
+        <div className="action-buttons-container">
+          <button className="gray-button">
+            <p className="lato gray-button-text">Get election reminders</p>
+          </button>
+          <button className="gray-button" onClick={startChallenge}>
+            <p className="lato gray-button-text">Take the challenge yourself</p>
+          </button>
+        </div>
+
+        <p className="lato tiny-text user-question">
+          Looking for something else?
+        </p>
+        <p className="lato tiny-text link-text restart">
+          <a className="lato" href="http://www.google.com">
+            Restart voter registration
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
