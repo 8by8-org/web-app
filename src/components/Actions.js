1ccbefdeb489d3ea4eabb6f24844dade4c46f632
mnarayan1 <86498656+mnarayan1@users.noreply.github.com>
2021-12-11 11:02:03 -0800

    1.2 & 3.1: redirection and styling for player flow (#60)
    * styling player flow
    
    * fixes
    
    * fixed action message and reminder path

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,36 +1,43 @@
 import React from 'react'
-import { Row, Button, Container } from 'react-bootstrap'
 import { useHistory } from 'react-router'
-import './Actions.css'
+import {ReactComponent as Avatar} from '../assets/avatars/Boy1.svg'
+import WhiteCurve from '../assets/images/Actions/Union.svg'
+import './Actions.scss'
 
 export default function Actions() {
     const history = useHistory();
+    const validChallenger = false; 
+
     return (
-        <div>
-            <Container fluid className="py-4 px-5">
-                <Row align="center">
-                    <h1>Take Action</h1>
-                </Row>
-                <Row align="center">
-                    <p>Your actions matter towards better AAPI representation in our country.</p>
-                </Row>
-                <Row>
-                    <p>user icon</p>
-                </Row>
-                <Row>
-                    <p>You're taking action for:</p>
-                    <p>(Get challenger info from url)</p>
-                </Row>
-                <Row className="py-2">
-                    <Button className="py-2" id="button-style" onClick={() => {history.push('/voterreg'); localStorage.setItem('player', 'voter')}}>Register to vote</Button>
-                </Row>
-                <Row className="py-2">
-                    <Button className="py-2" id="button-style" onClick={() => {localStorage.setItem('player', 'reminder')}}>Get election reminders</Button>
-                </Row>
-                <Row className="py-2">
-                    <Button className="py-2" id="button-style" onClick={() => {history.push('/challengerwelcome')}}>Take the challenge yourself</Button>
-                </Row>
-            </Container>
+        <div className="main-content">
+            <div className="top">
+                { validChallenger ? (
+                    <div>
+                        <div className="avatar-and-status" align="center">
+                            <div className="action-status">
+                                <h1 className="heading">Take Action For:</h1>
+                            </div>
+                            <div className="avatar-container">
+                                <Avatar id="challenger-avatar"/> {/**Avatar will change based on challenger*/}
+                                <p id="challenger-name">Name</p> {/**Get challenger name from url */}
+                            </div>
+                        </div>
+                        <img src={WhiteCurve} className="curve"/> 
+                    </div>
+                    ) : <h1 id="action-no-challenger" align="center">Take Action</h1>
+                }
+            </div>
+            <div className="action-items">
+                <div className="py-2">
+                    <button className="primary-button" onClick={() => {history.push('/voterreg'); localStorage.setItem('player', 'voter')}}>Register to vote</button>
+                </div>
+                <div className="py-2">
+                    <button className="secondary-button" onClick={() => {history.push('/election-reminders'); localStorage.setItem('player', 'reminder')}}>Get election reminders</button>
+                </div>
+                <div className="py-2">
+                    <button className="secondary-button" onClick={() => {history.push('/challengerwelcome')}}>Take the challenge</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/PlayerWelcome.js
+++ src/components/PlayerWelcome.js
@@ -1,22 +1,74 @@
-import { Button, Row, Container } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
+import SignUp1 from "../assets/images/PlayerWelcome/SignUp1.png"
+import SignUp2 from "../assets/images/PlayerWelcome/SignUp2.png"
+import Vote from "../assets/images/PlayerWelcome/Vote1.png"
+import Calendar from "../assets/images/PlayerWelcome/Complete1.svg"
+import Top from '../assets/images/PlayerWelcome/Top.svg'
+import './PlayerWelcome.scss'
 
 export default function PlayerWelcome() {
     const history = useHistory(); 
 
     return (
-        <div>
-            <Container fluid className="py-4 px-5">
-                <Row align="center">
-                    <h1>Welcome</h1>
-                </Row>
-                <Row>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p>
-                </Row>
-                <Row>
-                    <Button id="button-style" className="py-2" onClick={() => {history.push("/actions")}}>Let's Start</Button>
-                </Row>
-            </Container>
+        <div className="player-welcome">
+            <div className="top" align="center">
+                <img src={Top}/>
+            </div>
+            <div className="main-content">
+                <div>
+                    <h2 className="heading"><u className="underline">Support</u> Yang's 8by8 Challenge!</h2>
+                    <div align="center">
+                        <img src={Calendar} />
+                    </div>
+                </div>
+                <div className="text">
+                    <p>
+                        <b>
+                            Help "challenger" win their <u>8BY8 Challenge</u> by registering to vote or taking other ctions to #stopasianhate!
+                        </b>
+                    </p>
+                </div>
+                <div>
+                    <button onClick={() => {history.push("/actions")}}>Get Started</button>
+                </div>
+                <div align="center">
+                    <p className="small-text">Already have an account? <a href="/signin">Sign In</a></p>
+                </div>
+                <div>
+                    <h3 className="heading"><u className="underline">Here's how it works</u></h3>
+                </div>
+                <p className="subheading">
+                    1. ALL WE NEED IS YOUR ACTION
+                </p>
+                <p className="text">
+                    You can take any number of the available actions: register to vote, get election reminders, 
+                    or take the 8by8 challenge yourself. Pick one to start.
+                </p>
+                <div className="image">
+                        <img src={SignUp1} alt="8by8 Logo" />
+                </div>
+                <p className="subheading">
+                    2. YOUR FRIEND WILL EARN A BADGE
+                </p>
+                <p className="text">
+                    Any of the 3 actions will help your friend earn a badge, and get closer to winning the challenge.
+                </p>
+                <div className="image">
+                        <img src={SignUp2} alt="8by8 Logo" />
+                </div>
+                <p className="subheading">
+                    3. COME BACK AND TAKE MORE ACTIONS
+                </p>
+                <p className="text">
+                    Whether it is to help the same friend or a different one, the more action you take, the better! 
+                    Note that you can only help earn on badge per friend.     
+                </p>
+                <div className="image">
+                        <img src={Vote} alt="8by8 Logo" />
+                </div>
+                <button onClick={() => {history.push("/actions")}}>Get Started</button>
+                <p align="center" className="small-text">Already have an account? <a href="/signin">Sign In</a></p>
+            </div>
             </div>
     )
 }
