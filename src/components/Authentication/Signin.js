9f12c32f3b5e01e8fa272f88ba76b1c3595f6780
Devanshu Sen Pandey <66650721+Devanshusp@users.noreply.github.com>
2022-02-12 13:48:55 -0600

    Added Google ReCAPTCHA + Fixed avatar autoselect bug (#69)
    * added google recaptcha to signup
    
    * added recaptcha to signin
    
    * autoselect first avatar on signup

--- src/components/Authentication/Signin.js
+++ src/components/Authentication/Signin.js
@@ -14,122 +14,139 @@ import errorMessage from "./../../errorMessage";
 import { useHistory } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 import "./Signin.scss";
+import ReCAPTCHA from "react-google-recaptcha";
 import { dummyPassword } from "../../constants";
 
 const localStorageEmailKey = "verifyUserEmail";
 
 export default function Login() {
-	const { currentUser } = useAuth();
-	const history = useHistory();
-	const [error, setError] = useState(null);
-	const [message, setMessage] = useState(null);
-	const [emailVisible, setEmailVisible] = useState(true);
-	const [buttonMessage, setButtonMessage] = useState(" "); // leave blank to hide button
+  const { currentUser } = useAuth();
+  const history = useHistory();
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [emailVisible, setEmailVisible] = useState(true);
+  const [buttonMessage, setButtonMessage] = useState(" "); // leave blank to hide button
+  const [reCaptchaPassed, setReCaptchaPassed] = useState(false);
 
-	const emailRef = useRef();
-	const buttonRef = useRef();
-	useEffect(() => {
-		let isMounted = true;
-		if (currentUser) {
-			history.push("/progress");
-			return;
-		}
-		if (!auth.isSignInWithEmailLink(auth.getAuth(), window.location.href)) {
-			// login step 1
-			setButtonMessage("Sign In");
-			buttonRef.current.onclick = async function () {
-				const email = emailRef.current.value;
-				const login = async (email) => {
-					try {
-						await auth.signInWithEmailAndPassword(
-							auth.getAuth(),
-							email,
-							dummyPassword
-						);
-						window.localStorage.setItem(
-							localStorageEmailKey,
-							email
-						);
-						setEmailVisible(false);
-						setButtonMessage(null);
-						setMessage("Logging in...");
-					} catch (e) {
-						setError(errorMessage(e));
-					}
-				};
-				if (!email) {
-					setMessage("Missing email");
-				} else {
-					login(email);
-				}
-			};
-		} else {
-			// login step 2
-			const verifyEmail = async (email) => {
-				try {
-					await auth.signInWithEmailLink(
-						auth.getAuth(),
-						email,
-						window.location.href
-					);
-				} catch (e) {
-					setError(errorMessage(e));
-				}
-			};
+  const emailRef = useRef();
+  const buttonRef = useRef();
 
-			const storedEmail =
-				window.localStorage.getItem(localStorageEmailKey);
-			if (!storedEmail) {
-				setMessage("Please re-enter your email");
-				setButtonMessage("Verify email");
-				buttonRef.current.onclick = () =>
-					verifyEmail(emailRef.current.value);
-			} else {
-				verifyEmail(storedEmail);
-			}
-		}
-		// eslint-disable-next-line
-	}, [currentUser]);
+  useEffect(() => {
+    let isMounted = true;
+    if (currentUser) {
+      history.push("/progress");
+      return;
+    }
+    if (!auth.isSignInWithEmailLink(auth.getAuth(), window.location.href)) {
+      // login step 1
+      setButtonMessage("Sign In");
+      buttonRef.current.onclick = async function () {
+        const email = emailRef.current.value;
+        const login = async (email) => {
+          try {
+            await auth.signInWithEmailAndPassword(
+              auth.getAuth(),
+              email,
+              dummyPassword
+            );
+            window.localStorage.setItem(localStorageEmailKey, email);
+            setEmailVisible(false);
+            setButtonMessage(null);
+            setMessage("Logging in...");
+          } catch (e) {
+            setError(errorMessage(e));
+          }
+        };
+        if (!email) {
+          setMessage("Missing email");
+        } else {
+          login(email);
+        }
+      };
+    } else {
+      // login step 2
+      const verifyEmail = async (email) => {
+        try {
+          await auth.signInWithEmailLink(
+            auth.getAuth(),
+            email,
+            window.location.href
+          );
+        } catch (e) {
+          console.dir(e);
+          setError(errorMessage(e));
+        }
+      };
 
-	return (
-		<div className="signin p-3">
-			<Form className="d-grid signin-form">
-				<p className="signup-text">
-					<span className="signup-header">Sign in</span>
-					<br />
-					to continue your 8by8 journey
-				</p>
-				{error && <p className="error-col">{error}</p>}
-				{message && <p> {message} </p>}
-				{emailVisible && (
-					<div>
-						<Form.Control
-							className="form-control"
-							type="text"
-							placeholder="Name: "
-						></Form.Control>
-						<Form.Control
-							className="form-control"
-							type="email"
-							placeholder="Email:"
-							ref={emailRef}
-						></Form.Control>
-					</div>
-				)}
-				{buttonMessage && (
-					<Button className="button" ref={buttonRef}>
-						{buttonMessage}
-					</Button>
-				)}
-				{buttonMessage && (
-					<p className="signup-link">
-						Dont have an account? <> </>
-						<a href="/signup" style={{ style: "inline" }}>
-							Sign Up
-						</a>
-					</p>
-				)}
-			</Form>
-		</div>
-	);
+      const storedEmail = window.localStorage.getItem(localStorageEmailKey);
+      if (!storedEmail) {
+        setMessage("Please re-enter your email");
+        setButtonMessage("Verify email");
+        buttonRef.current.onclick = () => verifyEmail(emailRef.current.value);
+      } else {
+        verifyEmail(storedEmail);
+      }
+    }
+    // eslint-disable-next-line
+  }, [currentUser]);
+
+  return (
+    <div className="signin p-3">
+      <Form className="d-grid signin-form">
+        <p className="signup-text">
+          <span class="signup-header">Sign in</span>
+          <br />
+          to continue your 8by8 journey
+        </p>
+        {error && <p className="error-col">{error}</p>}
+        {message && <p> {message} </p>}
+        {emailVisible && (
+          <div>
+            <Form.Control
+              className="form-control"
+              type="text"
+              placeholder="Name: "
+            ></Form.Control>
+            <Form.Control
+              className="form-control"
+              type="email"
+              placeholder="Email:"
+              ref={emailRef}
+            ></Form.Control>
+          </div>
+        )}
+        <div className="recaptcha">
+          <ReCAPTCHA
+            sitekey="6LcVtjIeAAAAAEmNoh6l54YbfW8QoPm68aI8VJey"
+            onChange={() => {
+              setReCaptchaPassed(true);
+            }}
+            onExpired={() => {
+              setReCaptchaPassed(false);
+            }}
+            onErrored={() => {
+              setReCaptchaPassed(false);
+            }}
+          />
+        </div>
+        {buttonMessage && (
+          <Button
+            className="button"
+            ref={buttonRef}
+            disabled={!reCaptchaPassed}
+          >
+            {buttonMessage}
+          </Button>
+        )}
+        {buttonMessage && (
+          <p class="signup-link">
+            Dont have an account? <> </>
+            <a href="/signup" style={{ style: "inline" }}>
+              Sign Up
+            </a>
+          </p>
+        )}
+      </Form>
+    </div>
+  );
 }
--- src/components/Authentication/SignupPage.js
+++ src/components/Authentication/SignupPage.js
@@ -6,223 +6,242 @@ import errorMessage from "./../../errorMessage";
 import { useHistory } from "react-router-dom";
 import { dummyPassword } from "../../constants";
 import { Button, Form } from "react-bootstrap";
+import ReCAPTCHA from "react-google-recaptcha";
 import "./Signin.scss";
 const workingUrl = "localhost:3000";
 const db = getFirestore();
 export default function SignupPage() {
-	const { currentUser } = useAuth();
-	const history = useHistory();
-	const [error, setError] = useState(null);
-	const [message, setMessage] = useState(null);
-	const [emailVisible] = useState(true);
-	const [buttonMessage, setButtonMessage] = useState(" "); // leave blank to hide button
+  const { currentUser } = useAuth();
+  const history = useHistory();
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [emailVisible] = useState(true);
+  const [buttonMessage, setButtonMessage] = useState(" "); // leave blank to hide button
+  const [reCaptchaPassed, setReCaptchaPassed] = useState(false);
+  const [preselect, setPreselect] = useState(true);
 
-	const emailRef = useRef();
-	const confirmEmailRef = useRef();
-	const buttonRef = useRef();
-	const avatarRef = useRef();
-	const playerStatus = localStorage.getItem("player");
+  const emailRef = useRef();
+  const confirmEmailRef = useRef();
+  const buttonRef = useRef();
+  const avatarRef = useRef();
+  const playerStatus = localStorage.getItem("player");
 
-	useEffect(() => {
-		if (currentUser) {
-			history.push("/progress");
-			return;
-		}
+  useEffect(() => {
+    if (currentUser) {
+      history.push("/progress");
+      return;
+    }
 
-		if (currentUser && playerStatus === "voter") {
-			localStorage.removeItem("player");
-			history.push("/voterreg");
-			return;
-		}
+    if (currentUser && playerStatus === "voter") {
+      localStorage.removeItem("player");
+      history.push("/voterreg");
+      return;
+    }
 
-		if (currentUser && playerStatus === "reminder") {
-			localStorage.removeItem("player");
-			history.push("/electionreminder");
-			return;
-		}
+    if (currentUser && playerStatus === "reminder") {
+      localStorage.removeItem("player");
+      history.push("/electionreminder");
+      return;
+    }
 
-		if (currentUser && !playerStatus) {
-			history.push("/progress");
-			return;
-		}
+    if (currentUser && !playerStatus) {
+      history.push("/progress");
+      return;
+    }
 
-		if (!auth.isSignInWithEmailLink(auth.getAuth(), window.location.href)) {
-			// login step 1
-			setButtonMessage("Continue");
-			buttonRef.current.onclick = async function () {
-				const email = emailRef.current.value;
-				const confirmedEmail = confirmEmailRef.current.value;
-				const avatar = avatarRef.current.id;
-				const addAvatarToDB = async () => {
-					let user = auth.getAuth().currentUser;
-					let userRef = doc(db, "users", user.uid);
-					await updateDoc(userRef, {
-						avatar,
-					});
-				};
-				const createUser = async (email) => {
-					try {
-						// CryptoRandomString generates a random hash for the password (because it has no use right now)
-						await auth.createUserWithEmailAndPassword(
-							auth.getAuth(),
-							email,
-							dummyPassword
-						);
-						await addAvatarToDB();
-					} 
-					catch (e) {
-						setError(errorMessage(e));
-					}
-				};
-				if (!email) {
-					setError("Please enter an email address");
-				} 
-				else if(confirmedEmail !== email) {
-					setError("Emails do not match");
-				}
-				else {
-					createUser(email);
-					setMessage("Success");
-					window.location.href = (`${workingUrl}/login`);
-				}
-			};
-		}
-		// eslint-disable-next-line
-	}, [currentUser]);
+    if (!auth.isSignInWithEmailLink(auth.getAuth(), window.location.href)) {
+      // login step 1
+      setButtonMessage("Continue");
+      buttonRef.current.onclick = async function () {
+        const email = emailRef.current.value;
+        const confirmedEmail = confirmEmailRef.current.value;
+        const avatar = avatarRef.current.id;
+        const addAvatarToDB = async () => {
+          let user = auth.getAuth().currentUser;
+          let userRef = doc(db, "users", user.uid);
+          await updateDoc(userRef, {
+            avatar,
+          });
+        };
+        const createUser = async (email) => {
+          try {
+            // CryptoRandomString generates a random hash for the password (because it has no use right now)
+            await auth.createUserWithEmailAndPassword(
+              auth.getAuth(),
+              email,
+              dummyPassword
+            );
+            await addAvatarToDB();
+          } catch (e) {
+            setError(errorMessage(e));
+          }
+        };
+        if (!email) {
+          setError("Please enter an email address");
+        } else if (confirmedEmail !== email) {
+          setError("Emails do not match");
+        } else {
+          createUser(email);
+          setMessage("Success");
+          window.location.href = `${workingUrl}/login`;
+        }
+      };
+    }
+    // eslint-disable-next-line
+  }, [currentUser]);
 
-	return (
-		<>
-			<div className="signin p-3">
-				<Form className="d-grid signin-form">
-					<p className="signup-text">
-						<span className="signup-header">Sign up</span>
-						<br />
-						to start your 8by8 journey
-					</p>
-					{error && <p className="error-col" data-testid="error"> {error}</p>}
-					{message && <p> {message} </p>}
-					{emailVisible && (
-						<div>
-							<Form.Control
-								className="form-control name-field"
-								type="text"
-								placeholder="Name: "
-							></Form.Control>
-							<Form.Control
-								className="form-control email-field"
-								type="email"
-								placeholder="Email:"
-								ref={emailRef}
-							></Form.Control>
-							<Form.Control
-								className="form-control confirm-field"
-								type="email"
-								placeholder="Confirm Email:"
-								ref={confirmEmailRef}
-							></Form.Control>
-							<p className="signup-text signup-header">
-								Which One's you?{" "}
-							</p>
-							<input
-								type="radio"
-								id="0"
-								name="avatar"
-								value="0"
-								ref={avatarRef}
-							/>
-							<label htmlFor="0">
-								<div className="avatar">
-									<img
-										className="avatar-img"
-										src={
-											process.env.PUBLIC_URL +
-											"/avatars/avatar1.png"
-										}
-										alt=""
-									/>
-								</div>
-							</label>
-							<input
-								type="radio"
-								id="1"
-								name="avatar"
-								value="1"
-								ref={avatarRef}
-							/>
-							<label htmlFor="1">
-								<div className="avatar">
-									<img
-										className="avatar-img"
-										src={
-											process.env.PUBLIC_URL +
-											"/avatars/avatar2.png"
-										}
-										alt=""
-									/>
-								</div>
-							</label>
-							<br />
-							<input
-								type="radio"
-								id="2"
-								name="avatar"
-								value="2"
-								ref={avatarRef}
-							/>
-							<label htmlFor="2">
-								<div className="avatar">
-									<img
-										className="avatar-img"
-										src={
-											process.env.PUBLIC_URL +
-											"/avatars/avatar3.png"
-										}
-										alt=""
-									/>
-								</div>
-							</label>
-							<input
-								type="radio"
-								id="3"
-								name="avatar"
-								value="3"
-								ref={avatarRef}
-							/>
-							<label htmlFor="3">
-								<div className="avatar" data-testid="avatar">
-									<img
-										className="avatar-img"
-										src={
-											process.env.PUBLIC_URL +
-											"/avatars/avatar4.png"
-										}
-										alt=""
-									/>
-								</div>
-							</label>
-						</div>
-					)}
-					<br />
-					<p className="tos-text">
-						By clicking on Continue, I agree to the &#160; 
-						<a onClick = {() => history.push("/termsofservice")} className="link inline">Terms of Service </a> and the &#160;<a href="#" className="link inline">Privacy Policy</a>
-					</p>
-					<br />
-					{buttonMessage && (
-						<Button className="button" data-testid="continue-button" ref={buttonRef}>
-							{buttonMessage}
-						</Button>
-					)}
-					{buttonMessage && (
-						<p className="signin-link">
-							Have an account? <> </>
-							<a href="/signin" style={{ style: "inline" }}>
-								Sign In
-							</a>
-						</p>
-					)}
-				</Form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="signin p-3">
+        <Form className="d-grid signin-form">
+          <p className="signup-text">
+            <span class="signup-header">Sign up</span>
+            <br />
+            to start your 8by8 journey
+          </p>
+          {error && <p className="error-col">{error}</p>}
+          {message && <p> {message} </p>}
+          {emailVisible && (
+            <div>
+              <Form.Control
+                className="form-control name-field"
+                type="text"
+                placeholder="Name: "
+              ></Form.Control>
+              <Form.Control
+                className="form-control email-field"
+                type="email"
+                placeholder="Email:"
+                ref={emailRef}
+              ></Form.Control>
+              <Form.Control
+                className="form-control confirm-field"
+                type="email"
+                placeholder="Confirm Email:"
+                ref={confirmEmailRef}
+              ></Form.Control>
+              <p className="signup-text signup-header">Which One's you? </p>
+              <div className="avatar-container">
+                <input
+                  checked={preselect}
+                  type="radio"
+                  id="0"
+                  name="avatar"
+                  value="0"
+                  ref={avatarRef}
+                />
+                <label htmlFor="0">
+                  <div className="avatar">
+                    <img
+                      className="avatar-img"
+                      src={process.env.PUBLIC_URL + "/avatars/avatar1.png"}
+                      alt=""
+                    />
+                  </div>
+                </label>
+                <input
+                  onClick={() => setPreselect(null)}
+                  type="radio"
+                  id="1"
+                  name="avatar"
+                  value="1"
+                  ref={avatarRef}
+                />
+                <label htmlFor="1">
+                  <div className="avatar">
+                    <img
+                      className="avatar-img"
+                      src={process.env.PUBLIC_URL + "/avatars/avatar2.png"}
+                      alt=""
+                    />
+                  </div>
+                </label>
+                <br />
+                <input
+                  onClick={() => setPreselect(null)}
+                  type="radio"
+                  id="2"
+                  name="avatar"
+                  value="2"
+                  ref={avatarRef}
+                />
+                <label htmlFor="2">
+                  <div className="avatar">
+                    <img
+                      className="avatar-img"
+                      src={process.env.PUBLIC_URL + "/avatars/avatar3.png"}
+                      alt=""
+                    />
+                  </div>
+                </label>
+                <input
+                  onClick={() => setPreselect(null)}
+                  type="radio"
+                  id="3"
+                  name="avatar"
+                  value="3"
+                  ref={avatarRef}
+                />
+                <label htmlFor="3">
+                  <div className="avatar">
+                    <img
+                      className="avatar-img"
+                      src={process.env.PUBLIC_URL + "/avatars/avatar4.png"}
+                      alt=""
+                    />
+                  </div>
+                </label>
+              </div>
+            </div>
+          )}
+          <div className="recaptcha">
+            <ReCAPTCHA
+              sitekey="6LcVtjIeAAAAAEmNoh6l54YbfW8QoPm68aI8VJey"
+              onChange={() => {
+                setReCaptchaPassed(true);
+              }}
+              onExpired={() => {
+                setReCaptchaPassed(false);
+              }}
+              onErrored={() => {
+                setReCaptchaPassed(false);
+              }}
+            />
+          </div>
+          <br />
+          <p className="tos-text">
+            By clicking on Continue, I agree to the &#160;
+            <a
+              onClick={() => history.push("/termsofservice")}
+              className="link inline"
+            >
+              Terms of Service{" "}
+            </a>{" "}
+            and the &#160;
+            <a href="#" className="link inline">
+              Privacy Policy
+            </a>
+          </p>
+          <br />
+          {buttonMessage && (
+            <Button
+              className="button"
+              ref={buttonRef}
+              disabled={!reCaptchaPassed}
+            >
+              {buttonMessage}
+            </Button>
+          )}
+          {buttonMessage && (
+            <p class="signin-link">
+              Have an account? <> </>
+              <a href="/signin" style={{ style: "inline" }}>
+                Sign In
+              </a>
+            </p>
+          )}
+        </Form>
+      </div>
+    </>
+  );
 }
