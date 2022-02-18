8457b0b87a546668d9fc171c2f54ad5055829746
Devanshu Pandey <devanshusenp@gmail.com>
2022-02-18 12:33:50 -0600

    added firebase cloud function (needs testing) + signup styling

--- functions/index.js
+++ functions/index.js
@@ -2,52 +2,81 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
-
 const db = admin.firestore();
 
+// signup function
 exports.onSignup = functions.auth.user().onCreate(async (user) => {
-    async function generateUniqueInviteCode() {
-        const length = 8;
-        const dictionary = "234579abcdefghjkmnpqrstwxyz"; // unambiguous characters
+  async function generateUniqueInviteCode() {
+    const length = 8;
+    const dictionary = "234579abcdefghjkmnpqrstwxyz"; // unambiguous characters
 
-        const generateRandom = () => {
-            let out = "";
-            for (let i = 0; i < length; i++) {
-                out += dictionary.charAt(Math.floor(Math.random() * dictionary.length));
-            }
-            return out;
-        };
-        const checkIsRandom = (allegedRandom) => { // dumbest way of guaranteeing uniqueness but this will have to do for now
-            return new Promise((resolve, reject) => {
-                db.collection("users").where("inviteCode", "==", allegedRandom)
-                    .get()
-                    .then(querySnapshot => resolve(querySnapshot.empty))
-                    .catch(reject);
-            });
-        };
+    const generateRandom = () => {
+      let out = "";
+      for (let i = 0; i < length; i++) {
+        out += dictionary.charAt(Math.floor(Math.random() * dictionary.length));
+      }
+      return out;
+    };
+    const checkIsRandom = (allegedRandom) => {
+      // dumbest way of guaranteeing uniqueness but this will have to do for now
+      return new Promise((resolve, reject) => {
+        db.collection("users")
+          .where("inviteCode", "==", allegedRandom)
+          .get()
+          .then((querySnapshot) => resolve(querySnapshot.empty))
+          .catch(reject);
+      });
+    };
 
-        let currentRandom = generateRandom();
-        let timesRegenerated = 0;
-        while (!await checkIsRandom(currentRandom)) {
-            currentRandom = generateRandom();
-            if (++timesRegenerated >= 20) { // terminate before we drain away our resources
-                throw new Error("either by chance or something really bad happened, but 20 generated invite codes and still yielded no unique code");
-            }
-        }
-        if (timesRegenerated) {
-            functions.logger.warn(`encountered ${timesRegenerated} collisions`);
-        }
-        return currentRandom;
+    let currentRandom = generateRandom();
+    let timesRegenerated = 0;
+    while (!(await checkIsRandom(currentRandom))) {
+      currentRandom = generateRandom();
+      if (++timesRegenerated >= 20) {
+        // terminate before we drain away our resources
+        throw new Error(
+          "either by chance or something really bad happened, but 20 generated invite codes and still yielded no unique code"
+        );
+      }
     }
-
-    const userRef = db.collection("users").doc(user.uid);
-    if (!await userRef.get().exists) {
-        userRef.set({
-            email: user.email,
-            name: user.displayName,
-            inviteCode: await generateUniqueInviteCode(),
-            isRegisteredVoter: false,
-            lastActive: admin.firestore.FieldValue.serverTimestamp()
-        });
+    if (timesRegenerated) {
+      functions.logger.warn(`encountered ${timesRegenerated} collisions`);
     }
+    return currentRandom;
+  }
+
+  const userRef = db.collection("users").doc(user.uid);
+  if (!(await userRef.get().exists)) {
+    userRef.set({
+      email: user.email,
+      name: user.displayName,
+      inviteCode: await generateUniqueInviteCode(),
+      isRegisteredVoter: false,
+      lastActive: admin.firestore.FieldValue.serverTimestamp(),
+    });
+  }
+});
+
+// verifying recaptcha function (hasn't been implemented nor tested yet)
+import fetch from "node-fetch";
+exports.verifyReCaptcha = functions.https.onCall(async (data) => {
+  const secret_key = "6LcVtjIeAAAAAMu7qszTrL0zjbdK_2ClFPW2jUMU";
+  const response_key = data.response_key;
+
+  await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`,
+    {
+      method: "POST",
+    }
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      throw new functions.https.HttpsError(
+        "an error occured while calling the function: ",
+        error
+      );
+    });
 });
--- src/components/Authentication/SignupPage.js
+++ src/components/Authentication/SignupPage.js
@@ -11,12 +11,16 @@ import avatar2 from "../../assets/images/SignUpPage/avatar2.png";
 import avatar3 from "../../assets/images/SignUpPage/avatar3.png";
 import avatar4 from "../../assets/images/SignUpPage/avatar4.png";
 import ReCAPTCHA from "react-google-recaptcha";
-import "./Signin.scss";
+import { getFunctions, httpsCallable } from "firebase/functions";
+import "./SignupPage.scss";
 const db = getFirestore();
 
 export default function SignupPage() {
   const { currentUser } = useAuth();
   const history = useHistory();
+
+  const functions = getFunctions();
+  const verifyReCaptcha = httpsCallable(functions, "verifyReCaptcha");
 
   const [error, setError] = useState(null);
   const [message, setMessage] = useState(null);
@@ -56,7 +60,7 @@ export default function SignupPage() {
 
     // signup logic
     if (!auth.isSignInWithEmailLink(auth.getAuth(), window.location.href)) {
-      setButtonMessage("Continue");
+      setButtonMessage("Submit");
       buttonRef.current.onclick = async function () {
         const email = emailRef.current.value;
         const confirmedEmail = confirmEmailRef.current.value;
@@ -82,10 +86,10 @@ export default function SignupPage() {
               email,
               dummyPassword
             );
-            // waiting for user doc to be created before adding data
+            // waiting a few seconds for user doc to be created before adding data
             await setTimeout(() => {
               addUserToDB(username, avatarNum);
-            }, 2000);
+            }, 3000); // blame this if username & avatar aren't stored
           } catch (e) {
             setError(errorMessage(e));
           }
@@ -105,136 +109,140 @@ export default function SignupPage() {
   }, [currentUser]);
 
   return (
-    <>
-      <div className="signin p-3">
-        <Form className="d-grid signin-form">
-          <p className="signup-text">
-            <span class="signup-header">Sign up</span>
-            <br />
-            to start your 8by8 journey
-          </p>
-          {error && <p className="error-col">{error}</p>}
-          {message && <p> {message} </p>}
-          {emailVisible && (
-            <div>
-              <Form.Control
-                className="form-control name-field"
-                type="text"
-                placeholder="Name: "
-                ref={nameRef}
-              ></Form.Control>
-              <Form.Control
-                className="form-control email-field"
-                type="email"
-                placeholder="Email:"
-                ref={emailRef}
-              ></Form.Control>
-              <Form.Control
-                className="form-control confirm-field"
-                type="email"
-                placeholder="Confirm Email:"
-                ref={confirmEmailRef}
-              ></Form.Control>
-              <p className="signup-text signup-header">Which One's you? </p>
-              <div className="avatar-container">
-                <input checked={preselect} type="radio" name="avatar" id="1" />
-                <label htmlFor="1">
-                  <div className="avatar">
-                    <img className="avatar-img" src={avatar1} alt="avatar" />
-                  </div>
-                </label>
-                <input
-                  onClick={() => {
-                    setPreselect(null);
-                  }}
-                  type="radio"
-                  name="avatar"
-                  id="2"
-                />
-                <label htmlFor="2">
-                  <div className="avatar">
-                    <img className="avatar-img" src={avatar2} alt="avatar" />
-                  </div>
-                </label>
-                <br />
-                <input
-                  onClick={() => {
-                    setPreselect(null);
-                  }}
-                  type="radio"
-                  name="avatar"
-                  id="3"
-                />
-                <label htmlFor="3">
-                  <div className="avatar">
-                    <img className="avatar-img" src={avatar3} alt="avatar" />
-                  </div>
-                </label>
-                <input
-                  onClick={() => {
-                    setPreselect(null);
-                  }}
-                  type="radio"
-                  name="avatar"
-                  id="4"
-                />
-                <label htmlFor="4">
-                  <div className="avatar">
-                    <img className="avatar-img" src={avatar4} alt="avatar" />
-                  </div>
-                </label>
-              </div>
+    <div className="signup">
+      <p className="normal-title">Sign up</p>
+      <p className="no-underline-title">to start your 8by8 journey</p>
+
+      <Form className="form">
+        {error && <p className="error">{error}</p>}
+        {message && <p> {message} </p>}
+        {emailVisible && (
+          <>
+            <p className="required-text">*Required information</p>
+            <Form.Control
+              className="text-input"
+              type="text"
+              placeholder="Name*"
+              ref={nameRef}
+            ></Form.Control>
+            <Form.Control
+              className="text-input"
+              type="email"
+              placeholder="Email address*"
+              ref={emailRef}
+            ></Form.Control>
+            <Form.Control
+              className="text-input"
+              type="email"
+              placeholder="Re-enter Email address*"
+              ref={confirmEmailRef}
+            ></Form.Control>
+            <p className="small-title">Which One's you? </p>
+            <div className="avatar-container">
+              {/* avatar 1 */}
+              <input checked={preselect} type="radio" name="avatar" id="1" />
+              <label htmlFor="1">
+                <img className="avatar-img" src={avatar1} alt="avatar1" />
+              </label>
+
+              {/* avatar 2 */}
+              <input
+                onClick={() => {
+                  setPreselect(null);
+                }}
+                type="radio"
+                name="avatar"
+                id="2"
+              />
+              <label htmlFor="2">
+                <img className="avatar-img" src={avatar2} alt="avatar2" />
+              </label>
+              <br />
+
+              {/* avatar 3 */}
+              <input
+                onClick={() => {
+                  setPreselect(null);
+                }}
+                type="radio"
+                name="avatar"
+                id="3"
+              />
+              <label htmlFor="3">
+                <img className="avatar-img" src={avatar3} alt="avatar3" />
+              </label>
+
+              {/* avatar 4 */}
+              <input
+                onClick={() => {
+                  setPreselect(null);
+                }}
+                type="radio"
+                name="avatar"
+                id="4"
+              />
+              <label htmlFor="4">
+                <img className="avatar-img" src={avatar4} alt="avatar4" />
+              </label>
             </div>
-          )}
+          </>
+        )}
 
-          <div className="recaptcha">
-            <ReCAPTCHA
-              sitekey="6LcVtjIeAAAAAEmNoh6l54YbfW8QoPm68aI8VJey"
-              onChange={() => {
-                setReCaptchaPassed(true);
-              }}
-              onExpired={() => {
-                setReCaptchaPassed(false);
-              }}
-              onErrored={() => {
-                setReCaptchaPassed(false);
-              }}
-            />
-          </div>
+        <div className="recaptcha">
+          <ReCAPTCHA
+            sitekey="6LcVtjIeAAAAAEmNoh6l54YbfW8QoPm68aI8VJey"
+            // need to add server recaptcha check
+            // onChange={async (response) => {
+            //   await verifyReCaptcha({ response_key: response })
+            //     .then((result) => {
+            //       console.log(result);
+            //     })
+            //     .catch((error) => {
+            //       console.log(error);
+            //     });
+            // }}
+            onChange={() => {
+              setReCaptchaPassed(true);
+            }}
+            onExpired={() => {
+              setReCaptchaPassed(false);
+            }}
+            onErrored={() => {
+              setReCaptchaPassed(false);
+            }}
+          />
+        </div>
 
-          <p className="tos-text">
-            By clicking on Continue, I agree to the &#160;
-            <a
-              onClick={() => history.push("/termsofservice")}
-              className="link inline"
-            >
-              Terms of Service{" "}
-            </a>{" "}
-            and the &#160;
-            <a href="#" className="link inline">
-              Privacy Policy
+        <p className="tos">
+          By clicking on Continue, I agree to the &#160;
+          <a onClick={() => history.push("/termsofservice")} className="link">
+            Terms of Service
+          </a>{" "}
+          and the{" "}
+          <a href="#" className="link">
+            Privacy Policy
+          </a>
+        </p>
+
+        {buttonMessage && (
+          <Button
+            className="gradient-button"
+            ref={buttonRef}
+            disabled={!reCaptchaPassed}
+          >
+            {buttonMessage}
+          </Button>
+        )}
+
+        {buttonMessage && (
+          <p class="signin small-text">
+            Already have an account?{" "}
+            <a href="/signin" className="link">
+              Sign In
             </a>
           </p>
-          <br />
-          {buttonMessage && (
-            <Button
-              className="button"
-              ref={buttonRef}
-              disabled={!reCaptchaPassed}
-            >
-              {buttonMessage}
-            </Button>
-          )}
-          {buttonMessage && (
-            <p class="signin-link">
-              Have an account? <> </>
-              <a href="/signin" style={{ style: "inline" }}>
-                Sign In
-              </a>
-            </p>
-          )}
-        </Form>
-      </div>
-    </>
+        )}
+      </Form>
+    </div>
   );
 }
