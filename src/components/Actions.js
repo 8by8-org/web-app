964665491c33d10596620c602e303c682f9523c8
Devanshu Pandey <devanshusenp@gmail.com>
2022-04-12 16:53:13 -0500

    styling

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -9,7 +9,6 @@ import ConfettiAnimation from "./ConfettiAnimation";
 
 export default function Actions() {
   const history = useHistory();
-  const validChallenger = false;
 
   const [confettiAnimation, setConfettiAnimation] = useState();
 
@@ -32,64 +31,57 @@ export default function Actions() {
   }
 
   return (
-    <div className="Actions">
+    <div className="actions">
       {confettiAnimation}
-      <div className="main-content">
-        <div className="top">
-          {validChallenger ? (
-            <div>
-              <div className="avatar-and-status" align="center">
-                <div className="action-status">
-                  <h1 className="heading">Take Action For:</h1>
-                </div>
-                <div className="avatar-container">
-                  <Avatar id="challenger-avatar" />{" "}
-                  {/**Avatar will change based on challenger*/}
-                  <p id="challenger-name">Name</p>{" "}
-                  {/**Get challenger name from url */}
-                </div>
-              </div>
-              <img src={WhiteCurve} className="curve" />
+
+      <div className="top">
+        <div>
+          <div className="avatar-and-status" align="center">
+            <div className="action-status">
+              <h1 className="heading">Take Action For:</h1>
             </div>
-          ) : (
-            <h1 id="action-no-challenger" align="center">
-              Take Action
-            </h1>
-          )}
+            <div className="avatar-container">
+              <Avatar id="challenger-avatar" />{" "}
+              {/**Avatar will change based on challenger*/}
+              <p id="challenger-name">Name</p>{" "}
+              {/**Get challenger name from url */}
+            </div>
+          </div>
+          <img src={WhiteCurve} className="curve" />
         </div>
-        <div className="action-items">
-          <div className="py-2">
-            <Button
-              className="primary-button"
-              onClick={() => {
-                history.push("/voterreg");
-                localStorage.setItem("player", "voter");
-              }}
-            >
-              Register to vote
-            </Button>
-          </div>
-          <div className="py-2">
-            <Button
-              className="secondary-button"
-              onClick={() => {
-                history.push("/election-reminders");
-                localStorage.setItem("player", "reminder");
-              }}
-            >
-              Get election reminders
-            </Button>
-          </div>
-          <div className="py-2">
-            <Button
-              className="secondary-button"
-              onClick={() => {
-                history.push("/challengerwelcome");
-              }}
-            >
-              Take the challenge
-            </Button>
-          </div>
+      </div>
+      <div className="action-items">
+        <div className="py-2">
+          <Button
+            className="primary-button"
+            onClick={() => {
+              history.push("/voterreg");
+              localStorage.setItem("player", "voter");
+            }}
+          >
+            Register to vote
+          </Button>
+        </div>
+        <div className="py-2">
+          <Button
+            className="secondary-button"
+            onClick={() => {
+              history.push("/election-reminders");
+              localStorage.setItem("player", "reminder");
+            }}
+          >
+            Get election reminders
+          </Button>
+        </div>
+        <div className="py-2">
+          <Button
+            className="secondary-button"
+            onClick={() => {
+              history.push("/challengerwelcome");
+            }}
+          >
+            Take the challenge
+          </Button>
         </div>
       </div>
     </div>
