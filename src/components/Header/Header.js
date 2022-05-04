72c31d2eb241bd59e029d94853094554b6598182
projectr495836 <98628400+projectr495836@users.noreply.github.com>
2022-05-03 19:37:42 -0700

    different text for different type of users
    On the side bar, when user is not challenger, it will show "take the challenge". When user is a challenger, it will show "my challenge".

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,15 @@ import logo from "./../../assets/logos/white-logo.svg";
 import sidebarLogo from "./../../assets/logos/white-logo.svg";
 import "./Header.scss";
 
+
+
+
+import { getUserType } from "./../../functions/UserType.js";
+
+
 function Header() {
+  
+  console.log(getUserType())
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => {
     setSidebar(!sidebar);
@@ -27,38 +35,81 @@ function Header() {
   const greeting = "Hi There!";
 
   // all sidebar links lead to path: /
-  const SidebarData = [
-    {
-      title: "Take the Challenge",
-      path: "/",
-      icon: <GiIcons.GiJeweledChalice />,
-    },
-    {
-      title: "Take Action",
-      path: "/",
-      icon: <GiIcons.GiJeweledChalice />,
-    },
-    {
-      title: "Notifications",
-      path: "/",
-      icon: <GiIcons.GiJeweledChalice />,
-    },
-    {
-      title: "Share",
-      path: "/",
-      icon: <GiIcons.GiJeweledChalice />,
-    },
-    {
-      title: "Why 8by8?",
-      path: "/",
-      icon: <GiIcons.GiJeweledChalice />,
-    },
-    {
-      title: "FAQS",
-      path: "/",
-      icon: <GiIcons.GiJeweledChalice />,
-    },
-  ];
+  var SidebarData;
+  var flagForTestNonChallenger=0;// make it non 0 if want to test the case for User Type is not challenger
+  if(getUserType()=="challenger" & flagForTestNonChallenger==0){
+
+    SidebarData = [
+      {
+        title: "My Challenge",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "Take Action",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "Notifications",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "Share",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "Why 8by8?",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "FAQS",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+    ];
+  }else{
+    SidebarData = [
+      {
+        title: "Take the Challenge",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "Take Action",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "Notifications",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "Share",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "Why 8by8?",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+      {
+        title: "FAQS",
+        path: "/",
+        icon: <GiIcons.GiJeweledChalice />,
+      },
+    ];
+    
+  }
+
+  // if(getUserType()=="challenger"){
+  //   SidebarData=SidebarDataForChallenger;
+  // }
 
   // static notification data for testing
   const NotifData = [
