1806102455779ca8d687166c504981a6bf965cc1
Devanshu Pandey <devanshusenp@gmail.com>
2022-05-05 19:59:41 -0500

    no progress page reload if share badge earned

--- src/components/Invite.js
+++ src/components/Invite.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { Nav } from "react-bootstrap";
 import "./Invite.scss";
@@ -10,7 +10,7 @@ import {
   WhatsappShareButton,
   EmailShareButton,
 } from "react-share";
-import { completedAction, delay } from "../functions/UserData"
+import { completedAction, delay, getUserDatabase } from "../functions/UserData";
 import CalendarSvg from "../assets/images/Invite/Calendar.svg";
 import FacebookSvg from "../assets/images/Invite/Facebook.svg";
 import TwitterSvg from "../assets/images/Invite/Twitter.svg";
@@ -22,10 +22,11 @@ import EmailSvg from "../assets/images/Invite/Email.svg";
 import TextSvg from "../assets/images/Invite/Text.svg";
 import Instagram from "./Instagram";
 
-function Invite({ toggleInvite , isShare}) {
+function Invite({ toggleInvite, isShare }) {
   const { currentUser } = useAuth();
-  const [ url, setUrl ] = useState(null);
+  const [url, setUrl] = useState(null);
   const [show, setShow] = useState(false);
+  const [shared, setShared] = useState(false);
 
   React.useEffect(() => {
     toggleInvite.current = changeShow;
@@ -40,11 +41,22 @@ function Invite({ toggleInvite , isShare}) {
     navigator.clipboard.writeText(shareUrl);
   }
 
+  useEffect(() => {
+    getUserDatabase().then((data) => {
+      if (data.sharedChallenge) {
+        setShared(true);
+      }
+    });
+  }, []);
+
   function reloadPage() {
-    window.location.reload();
+    if (!shared) {
+      window.location.reload();
+    }
   }
+
   function generateUrl() {
-    setUrl(`${window.location.origin}/share/${currentUser.uid}`)
+    setUrl(`${window.location.origin}/share/${currentUser.uid}`);
   }
 
   // shareUrl is currently a temporary placeholder for UID link
@@ -63,65 +75,110 @@ function Invite({ toggleInvite , isShare}) {
       <nav className={show ? "invite-menu active" : "invite-menu"}>
         <li className="invite-toggle">
           <Nav.Link to="#" id="close-icon">
-            <MdIcons.MdClose onClick={()=>{changeShow(); reloadPage()}} />
+            <MdIcons.MdClose
+              onClick={() => {
+                changeShow();
+                reloadPage();
+              }}
+            />
           </Nav.Link>
-          <span className="invite-title">{isShare ? 'Share' : 'Invite Friends'}</span>
+          <span className="invite-title">
+            {isShare ? "Share" : "Invite Friends"}
+          </span>
         </li>
 
         <div className="info">
           <img src={CalendarSvg} width="225px" />
           <p>
-            {isShare ?
-            'Share about your action. Invite friends to help Yang’s challenge and learn more about the 8by8 cause! If you are curious, preview what they’ll see.' :
-            'Invite friends to support your challenge by registering to vote, get election reminders or take the 8by8 challenge themselves.'
-            }
+            {isShare
+              ? "Share about your action. Invite friends to help Yang’s challenge and learn more about the 8by8 cause! If you are curious, preview what they’ll see."
+              : "Invite friends to support your challenge by registering to vote, get election reminders or take the 8by8 challenge themselves."}
           </p>
         </div>
 
         <div className="section social-media">
-          <p className="sub-heading">{isShare ? 'Social Media' : 'Copy your unique link'}</p>
-          <FacebookShareButton url={shareUrl} quote={quote} hashtag={hashtag}
-              onShareWindowClose={()=>{completedAction("share challenge")}}>
+          <p className="sub-heading">
+            {isShare ? "Social Media" : "Copy your unique link"}
+          </p>
+          <FacebookShareButton
+            url={shareUrl}
+            quote={quote}
+            hashtag={hashtag}
+            onShareWindowClose={() => {
+              completedAction("share challenge");
+            }}
+          >
             <img className="invite-icon" src={FacebookSvg} />
             <p className="invite-icon-label">Facebook</p>
           </FacebookShareButton>
 
-          <TwitterShareButton url={shareUrl} title={quote} 
-            onShareWindowClose={()=>{completedAction("share challenge")}}>
+          <TwitterShareButton
+            url={shareUrl}
+            title={quote}
+            onShareWindowClose={() => {
+              completedAction("share challenge");
+            }}
+          >
             <img className="invite-icon" src={TwitterSvg} />
             <p className="invite-icon-label">Twitter</p>
           </TwitterShareButton>
 
-          <button className="instagram-button" onClick={() => {
-            if (navigator.clipboard) {
-              copyToClipboard();
-            }
-            toggleIG.current();
-          }}>
+          <button
+            className="instagram-button"
+            onClick={() => {
+              if (navigator.clipboard) {
+                copyToClipboard();
+              }
+              toggleIG.current();
+            }}
+          >
             <img className="invite-icon" src={InstagramSvg} />
             <p className="invite-icon-label">Instagram</p>
           </button>
         </div>
 
         <div className="section messaging">
-
-          <p className="sub-heading">{isShare ? 'Messaging' : 'Copy your unique link'}</p>
-          <FacebookMessengerShareButton url={shareUrl} appId={appId} onShareWindowClose={()=>{completedAction("share challenge")}}>
+          <p className="sub-heading">
+            {isShare ? "Messaging" : "Copy your unique link"}
+          </p>
+          <FacebookMessengerShareButton
+            url={shareUrl}
+            appId={appId}
+            onShareWindowClose={() => {
+              completedAction("share challenge");
+            }}
+          >
             <img className="invite-icon" src={FacebookMessengerSvg} />
             <p className="invite-icon-label">Messenger</p>
           </FacebookMessengerShareButton>
 
-          <WhatsappShareButton url={shareUrl} title={quote} onShareWindowClose={()=>{completedAction("share challenge")}}>
-            <img className="invite-icon" src={WhatsAppSvg}/>
+          <WhatsappShareButton
+            url={shareUrl}
+            title={quote}
+            onShareWindowClose={() => {
+              completedAction("share challenge");
+            }}
+          >
+            <img className="invite-icon" src={WhatsAppSvg} />
             <p className="invite-icon-label">WhatsApp</p>
           </WhatsappShareButton>
 
-          <EmailShareButton url={shareUrl} subject={quote} body={body} beforeOnClick={()=>{completedAction("share challenge")}}>
+          <EmailShareButton
+            url={shareUrl}
+            subject={quote}
+            body={body}
+            beforeOnClick={() => {
+              completedAction("share challenge");
+            }}
+          >
             <img className="invite-icon" src={EmailSvg} />
             <p className="invite-icon-label">Email</p>
           </EmailShareButton>
-          
-          <button className="sms-button" onClick={() => window.open('sms:&body=' + body + ' ' + shareUrl)}>
+
+          <button
+            className="sms-button"
+            onClick={() => window.open("sms:&body=" + body + " " + shareUrl)}
+          >
             <img className="invite-icon" src={TextSvg} />
             <p className="invite-icon-label">Text</p>
           </button>
@@ -130,18 +187,21 @@ function Invite({ toggleInvite , isShare}) {
         <div className="section copy-link">
           <p className="sub-heading">Copy your unique link</p>
           <div className="link-container">
-
             <div className="unique-link">
               <p>{shareUrl}</p>
               <div className="underline"></div>
             </div>
-            <button onClick={() => {
-              if (navigator.clipboard) {
-                copyToClipboard(); 
-                completedAction("share challenge")}
-            }}>COPY</button>
+            <button
+              onClick={() => {
+                if (navigator.clipboard) {
+                  copyToClipboard();
+                  completedAction("share challenge");
+                }
+              }}
+            >
+              COPY
+            </button>
           </div>
-          
         </div>
       </nav>
       <Instagram toggleIG={toggleIG} isShare={isShare} shareUrl={shareUrl} />
