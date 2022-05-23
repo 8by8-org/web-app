7f29734d60316973f249394cd4a8fb73f7bb7d2d
Devanshu Pandey <devanshusenp@gmail.com>
2022-05-23 18:14:20 -0500

    get challenger data in actions page without local storage

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { getUserDatabase } from "./../../functions/UserData";
+import {
+  getChallengerDatabase,
+  getUserDatabase,
+} from "./../../functions/UserData";
 import { Button } from "react-bootstrap";
 import { useHistory } from "react-router";
 import { useAuth } from "../../contexts/AuthContext";
@@ -12,13 +15,14 @@ import Crown from "./../../assets/images/Actions/Crown.svg";
 import ConfettiAnimation from "./../ConfettiAnimation";
 import Invite from "../Invite.js";
 import "./Actions.scss";
+import { LoadingWheel } from "../LoadingWheel/LoadingWheel.component";
 
 const avatars = [Avatar1, Avatar2, Avatar3, Avatar4];
 
 export default function Actions() {
   const history = useHistory();
   const { currentUser } = useAuth();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [challengerInfo, setChallengerInfo] = useState(null);
   const [registeredVoter, setRegisteredVoter] = useState(false);
   const [notifyElectionReminders, setNotifyElectionReminders] = useState(false);
@@ -33,9 +37,19 @@ export default function Actions() {
       if (currentUser) {
         fetchUserData();
       }
-      setLoading(false);
-    }
-    else {
+    } else if (currentUser) {
+      getUserDatabase().then((data) => {
+        if (data.invitedBy.length > 0) {
+          getChallengerDatabase().then((data) => {
+            setChallengerInfo(data);
+            console.log(data);
+            fetchUserData();
+          });
+        } else {
+          history.push(`/signin`);
+        }
+      });
+    } else {
       history.push(`/signin`);
     }
   }, [loading]);
@@ -46,197 +60,290 @@ export default function Actions() {
         setRegisteredVoter(data.isRegisteredVoter);
         setStartedChallenge(data.startedChallenge);
         setNotifyElectionReminders(data.notifyElectionReminders);
+        setLoading(true);
       })
       .catch((e) => console.log(e));
   }
 
-  return (
-    loading === false ?
-      <div>
-        {/* if all three actions are completed */}
-        {registeredVoter && notifyElectionReminders && startedChallenge ?
-          <div className="actions">
-            <ConfettiAnimation time={8000} />
+  return loading ? (
+    <div>
+      {/* if all three actions are completed */}
+      {registeredVoter && notifyElectionReminders && startedChallenge ? (
+        <div className="actions">
+          <ConfettiAnimation time={8000} />
 
-            <div className="top">
-              <div className="avatar-and-status-finished">
-                <div className="action-status-finished">
-                  <h1 className="heading">
-                    You are done!<br/>
-                    You Supported:
-                  </h1>
-                </div>
-
-                <div className="avatar-container">
-                  <div className="images">
-                    <img
-                      alt="Challenger Avatar"
-                      src={challengerInfo.avatar ? avatars[(challengerInfo.avatar)-1] : avatars[0]}
-                      id="challenger-avatar"
-                    />
-                  </div>
-
-                  <p id="challenger-name">{challengerInfo.name}</p>
-                </div>
-              </div>
-            </div>
-
-            <img alt="White Curve" src={WhiteCurve} className="curve"/>
-
-            <div className="action-items">
-              <div className="py-2">
-                <Button
-                  className="primary-button"
-                  onClick={() => {history.push(`/signin`)}}
-                >
-                  See Your Challenge
-                </Button>
+          <div className="top">
+            <div className="avatar-and-status-finished">
+              <div className="action-status-finished">
+                <h1 className="heading">
+                  You are done!
+                  <br />
+                  You Supported:
+                </h1>
               </div>
 
-              <div className="py-2">
-                <Button
-                  className="secondary-button"
-                  onClick={() => toggleInvite.current()}
-                >
-                  Share About Your Actions
-                </Button>
+              <div className="avatar-container">
+                <div className="images">
+                  <img
+                    alt="Challenger Avatar"
+                    src={
+                      challengerInfo.avatar
+                        ? avatars[challengerInfo.avatar - 1]
+                        : avatars[0]
+                    }
+                    id="challenger-avatar"
+                  />
+                </div>
+
+                <p id="challenger-name">{challengerInfo.name}</p>
               </div>
             </div>
           </div>
-        :
-          // when there are actions that can still be completed by the user
-          <div className="actions">
-            <div className="top">
-              <div className="avatar-and-status">
-                <div className="action-status">
-                  {registeredVoter || notifyElectionReminders || startedChallenge ?
-                    <h1 className="heading">You Took Action!</h1>
-                  :
-                    <h1 className="heading">Take Action For:</h1>
-                  }
-                </div>
 
-                <div className="avatar-container">
-                  <div className="images">
-                    <img
-                      alt="Challenger Avatar"
-                      src={challengerInfo.avatar ? avatars[(challengerInfo.avatar)-1] : avatars[0]}
-                      id="challenger-avatar"
-                    />
-                    {(registeredVoter || notifyElectionReminders || startedChallenge) &&
-                      <div id="challenger-crown">
-                        <img alt="Challenger Crown" src={Crown} className="crown"/>
-                      </div>
-                    }
-                  </div>
+          <img alt="White Curve" src={WhiteCurve} className="curve" />
 
-                  <p id="challenger-name">{challengerInfo.name}</p>
-                </div>
-              </div>
+          <div className="action-items">
+            <div className="py-2">
+              <Button
+                className="primary-button"
+                onClick={() => {
+                  history.push(`/signin`);
+                }}
+              >
+                See Your Challenge
+              </Button>
             </div>
 
-            <img alt="White Curve" src={WhiteCurve} className="curve"/>
+            <div className="py-2">
+              <Button
+                className="secondary-button"
+                onClick={() => toggleInvite.current()}
+              >
+                Share About Your Actions
+              </Button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        // when there are actions that can still be completed by the user
+        <div className="actions">
+          <div className="top">
+            <div className="avatar-and-status">
+              <div className="action-status">
+                {registeredVoter ||
+                notifyElectionReminders ||
+                startedChallenge ? (
+                  <h1 className="heading">You Took Action!</h1>
+                ) : (
+                  <h1 className="heading">Take Action For:</h1>
+                )}
+              </div>
 
-            <div className="action-items">
-              {/* when only Take the challenge action is left */}
-              {registeredVoter && notifyElectionReminders && !startedChallenge ?
-                <h6 className="subheading">
-                  OTHER ACTIONS YOU CAN TAKE TO<br/>
-                  HELP THE AAPI COMMUNITY
-                </h6>
-              :
-                // when there is at least one action completed
-                (registeredVoter || notifyElectionReminders || startedChallenge) &&
-                  <h6 className="subheading">
-                    {challengerInfo.name} GOT A BADGE!<br/>
-                    HERE ARE OTHER ACTIONS TO HELP<br/>
-                    THE AAPI COMMUNITY<br/>
-                  </h6>
-              }
-
-              {/* action buttons only displayed when they are not completed */}
-              {!registeredVoter &&
-                <div className="py-2">
-                  <Button
-                    className="primary-button"
-                    onClick={() => {history.push(`/voterreg`); localStorage.setItem('player', 'voterreg')}}
-                  >
-                    Register to vote
-                  </Button>
+              <div className="avatar-container">
+                <div className="images">
+                  <img
+                    alt="Challenger Avatar"
+                    src={
+                      challengerInfo.avatar
+                        ? avatars[challengerInfo.avatar - 1]
+                        : avatars[0]
+                    }
+                    id="challenger-avatar"
+                  />
+                  {(registeredVoter ||
+                    notifyElectionReminders ||
+                    startedChallenge) && (
+                    <div id="challenger-crown">
+                      <img
+                        alt="Challenger Crown"
+                        src={Crown}
+                        className="crown"
+                      />
+                    </div>
+                  )}
                 </div>
-              }
 
-              {!notifyElectionReminders &&
+                <p id="challenger-name">{challengerInfo.name}</p>
+              </div>
+            </div>
+          </div>
+
+          <img alt="White Curve" src={WhiteCurve} className="curve" />
+
+          <div className="action-items">
+            {/* when only Take the challenge action is left */}
+            {registeredVoter && notifyElectionReminders && !startedChallenge ? (
+              <h6 className="subheading">
+                OTHER ACTIONS YOU CAN TAKE TO
+                <br />
+                HELP THE AAPI COMMUNITY
+              </h6>
+            ) : (
+              // when there is at least one action completed
+              (registeredVoter ||
+                notifyElectionReminders ||
+                startedChallenge) && (
+                <h6 className="subheading">
+                  {challengerInfo.name} GOT A BADGE!
+                  <br />
+                  HERE ARE OTHER ACTIONS TO HELP
+                  <br />
+                  THE AAPI COMMUNITY
+                  <br />
+                </h6>
+              )
+            )}
+
+            {/* action buttons only displayed when they are not completed */}
+            {!registeredVoter && (
               <div className="py-2">
                 <Button
-                  className={registeredVoter ? "primary-button" : "secondary-button"}
-                  onClick={() => {history.push(`/election-reminders`); localStorage.setItem('player', 'election-reminders')}}
+                  className="primary-button"
+                  onClick={() => {
+                    history.push(`/voterreg`);
+                    localStorage.setItem("player", "voterreg");
+                  }}
+                >
+                  Register to vote
+                </Button>
+              </div>
+            )}
+
+            {!notifyElectionReminders && (
+              <div className="py-2">
+                <Button
+                  className={
+                    registeredVoter ? "primary-button" : "secondary-button"
+                  }
+                  onClick={() => {
+                    history.push(`/election-reminders`);
+                    localStorage.setItem("player", "election-reminders");
+                  }}
                 >
                   Get election reminders
                 </Button>
               </div>
-              }
+            )}
 
-              {!startedChallenge &&
+            {!startedChallenge && (
               <div className="py-2">
                 <Button
-                  className={registeredVoter && notifyElectionReminders ? "primary-button" : "secondary-button"}
-                  onClick={() => {history.push('/challengerwelcome'); localStorage.setItem('player', 'progress')}}
+                  className={
+                    registeredVoter && notifyElectionReminders
+                      ? "primary-button"
+                      : "secondary-button"
+                  }
+                  onClick={() => {
+                    history.push("/challengerwelcome");
+                    localStorage.setItem("player", "progress");
+                  }}
                 >
                   Take the challenge
                 </Button>
               </div>
-              }
+            )}
 
-              {registeredVoter && notifyElectionReminders && !startedChallenge ?
-                // when only Take the challenge action is left
-                <div>
-                  <h6 className="text">
-                    Voting matters towards better AAPI<br/>
-                    representation in our country. You can help<br/>
-                    the community by encouring others to<br/>
-                    register! Take the challenge on your own!<br/>
-                  </h6>
-                  <div className="links-container">
-                    <a href="/homepage" className="links">What is 8by8 Challenge?</a>
-                    <button type="button" className="link-share" onClick={() => toggleInvite.current()}>Share about your actions</button>
-                  </div>
+            {registeredVoter && notifyElectionReminders && !startedChallenge ? (
+              // when only Take the challenge action is left
+              <div>
+                <h6 className="text">
+                  Voting matters towards better AAPI
+                  <br />
+                  representation in our country. You can help
+                  <br />
+                  the community by encouring others to
+                  <br />
+                  register! Take the challenge on your own!
+                  <br />
+                </h6>
+                <div className="links-container">
+                  <a href="/homepage" className="links">
+                    What is 8by8 Challenge?
+                  </a>
+                  <button
+                    type="button"
+                    className="link-share"
+                    onClick={() => toggleInvite.current()}
+                  >
+                    Share about your actions
+                  </button>
                 </div>
-              :
-                // when at least one action is completed
-                <div>
-                  {/* these are for when only that specific action is completed */}
-                  {registeredVoter && !notifyElectionReminders && !startedChallenge && <h6 className="text">Thanks for registering to vote!</h6>}
+              </div>
+            ) : (
+              // when at least one action is completed
+              <div>
+                {/* these are for when only that specific action is completed */}
+                {registeredVoter &&
+                  !notifyElectionReminders &&
+                  !startedChallenge && (
+                    <h6 className="text">Thanks for registering to vote!</h6>
+                  )}
 
-                  {!registeredVoter && notifyElectionReminders && !startedChallenge && <h6 className="text">Thanks for getting election reminders!</h6>}
+                {!registeredVoter &&
+                  notifyElectionReminders &&
+                  !startedChallenge && (
+                    <h6 className="text">
+                      Thanks for getting election reminders!
+                    </h6>
+                  )}
 
-                  {!registeredVoter && !notifyElectionReminders && startedChallenge && <h6 className="text">Thanks for taking the challenge!</h6>}
+                {!registeredVoter &&
+                  !notifyElectionReminders &&
+                  startedChallenge && (
+                    <h6 className="text">Thanks for taking the challenge!</h6>
+                  )}
+
+                {/* this is for when registered to vote or election reminders are turned on and challenge has been started */}
+                {(registeredVoter || notifyElectionReminders) &&
+                  startedChallenge && (
+                    <h6 className="text">Thanks for your actions!</h6>
+                  )}
+
+                <div className="links-container">
+                  {/* this is for when registered to vote or election reminders are turned on */}
+                  {((registeredVoter &&
+                    !notifyElectionReminders &&
+                    !startedChallenge) ||
+                    (!registeredVoter &&
+                      notifyElectionReminders &&
+                      !startedChallenge)) && (
+                    <button
+                      type="button"
+                      className="link-share"
+                      onClick={() => toggleInvite.current()}
+                    >
+                      Share about your action
+                    </button>
+                  )}
 
                   {/* this is for when registered to vote or election reminders are turned on and challenge has been started */}
-                  {((registeredVoter || notifyElectionReminders) && startedChallenge) &&
-                    <h6 className="text">Thanks for your actions!</h6>}
+                  {(registeredVoter || notifyElectionReminders) &&
+                    startedChallenge && (
+                      <button
+                        type="button"
+                        className="link-share"
+                        onClick={() => toggleInvite.current()}
+                      >
+                        Share about your actions
+                      </button>
+                    )}
 
-                  <div className="links-container">
-                    {/* this is for when registered to vote or election reminders are turned on */}
-                    {((registeredVoter && !notifyElectionReminders && !startedChallenge) ||
-                      (!registeredVoter && notifyElectionReminders && !startedChallenge)) &&
-                      <button type="button" className="link-share" onClick={() => toggleInvite.current()}>Share about your action</button>}
-
-                    {/* this is for when registered to vote or election reminders are turned on and challenge has been started */}
-                    {((registeredVoter || notifyElectionReminders) && startedChallenge) &&
-                      <button type="button" className="link-share" onClick={() => toggleInvite.current()}>Share about your actions</button>}
-
-                    {/* this is for when user has started their own challenge */}
-                    {startedChallenge && <a href="/signin" className="links">See your challenge</a>}
-                  </div>
+                  {/* this is for when user has started their own challenge */}
+                  {startedChallenge && (
+                    <a href="/signin" className="links">
+                      See your challenge
+                    </a>
+                  )}
                 </div>
-              }
-            </div>
+              </div>
+            )}
           </div>
-        }
+        </div>
+      )}
 
-        <Invite toggleInvite={toggleInvite} isShare={true} />
-      </div>
-    : <h1>loading</h1>
-  )
+      <Invite toggleInvite={toggleInvite} isShare={true} />
+    </div>
+  ) : (
+    <LoadingWheel overlay={false} />
+  );
 }
