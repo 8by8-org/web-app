a29e2a877fa99b9c8407262115927cf684715350
Naim <islamm7541@gmail.com>
2022-05-25 02:02:41 -0400

    Update /actions page #75 (Finish): Added in share. (#130)
    * Added in Preview pop-up in the invite component. Added alt prop to images in instagram, invite, and playerwelcome compoents. Fixed a bug in playerwelcome page that wasn't geting the correct information of the person that the player is doing actions for.
    
    * Fixed the styling that I messed up for sign in buttons on the playerwelcome page.
    
    * Added header and footer to the preview pop-up and made sure their buttons don't work when on preview. Fixed up styling issues with preview. Added comments to footer, header, instagram, invite, and preview components.
    
    * Fixed bug with navbar being in front of main content but invisible. In share above the player link, change text to include the challenger's name. Fixed bug where the challengerinfo was being changed on the invite component causeing the share link to be wrong.
    
    * Added a react library to dynamicaly change the meta tags. Added in all the different images to be shared on socail media. If the user is loggedin and there is no challengerinfo in local storage then actions component uses the loggedin user refferedby userId.
    
    * Imports for playerwelcome were not correctly tracked. Header report bugs link changed to button.
    
    * Fixed styling issue for report button and preview header. Fixed invite error for share when user is not logged in.

--- src/components/PlayerWelcome/PlayerWelcome.js
+++ src/components/PlayerWelcome/PlayerWelcome.js
@@ -1,147 +1,134 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 import { Button } from "react-bootstrap";
-import { getFirestore, getDoc, doc } from "firebase/firestore";
-import SignUp1 from "./../../assets/images/PlayerWelcome/SignUp1.png";
-import SignUp2 from "./../../assets/images/PlayerWelcome/SignUp2.png";
-import Vote from "./../../assets/images/PlayerWelcome/Vote1.png";
-import BlackCurve from "./../../assets/images/PlayerWelcome/BlackCurve.svg";
-import "./PlayerWelcome.scss";
-import { auth } from "./../../firebase";
-import { dummyPassword } from "./../../constants";
-import { useAuth } from "./../../contexts/AuthContext";
-import { LoadingWheel } from "./../LoadingWheel/LoadingWheel.component";
+import { getFirestore, getDoc, doc } from 'firebase/firestore'
+import SignUp1 from "../../assets/images/PlayerWelcome/SignUp1.png"
+import SignUp2 from "../../assets/images/PlayerWelcome/SignUp2.png"
+import Vote from "../../assets/images/PlayerWelcome/Vote1.png"
+import BlackCurve from '../../assets/images/PlayerWelcome/BlackCurve.svg'
+import './PlayerWelcome.scss'
+import { auth } from "../../firebase";
+import { dummyPassword } from "../../constants";
+import { useAuth } from "../../contexts/AuthContext";
+import { LoadingWheel } from "../LoadingWheel/LoadingWheel.component";
 
-export default function PlayerWelcome() {
-  const history = useHistory();
-  const { currentUser } = useAuth();
-  const [loading, setLoading] = useState(true);
-  const [challengerInfo, setChallengerInfo] = useState(null);
+export default function PlayerWelcome({ isShare }) {
+    const history = useHistory();
+    const { currentUser } = useAuth();
+    const [ loading, setLoading ] = useState(true);
+    const [ challengerInfo, setChallengerInfo ] = useState(null);
 
-  const url = window.location.href;
-  const code = url.split("/").pop();
+    const url = window.location.href
+    const code = url.split('/').pop();
 
-  async function getChallengerInfo() {
-    const db = getFirestore();
-    const docRef = doc(db, "users", code);
+    async function getChallengerInfo() {
+        const db = getFirestore();
+        // If isShare is false then use the uid of the current user, else use the code gotten from the url.
+        const docRef = doc(db, "users", isShare === false ? currentUser.uid : code)
 
-    //only log in with substitute user if not already authenticated
-    //only challenger's name and avatar is stored for security
-    if (currentUser) {
-      const query = await getDoc(docRef);
-      const info = (({ name, avatar }) => ({ name, avatar }))(query.data());
-      info.challengerID = code;
-      localStorage.setItem("challengerInfo", JSON.stringify(info));
-    } else {
-      await auth.signInWithEmailAndPassword(
-        auth.getAuth(),
-        "wkvxgesiknosbamhei@kvhrr.com",
-        dummyPassword
-      );
-      const query = await getDoc(docRef);
-      const info = (({ name, avatar }) => ({ name, avatar }))(query.data());
-      info.challengerID = code;
-      localStorage.setItem("challengerInfo", JSON.stringify(info));
-      auth.getAuth().signOut();
+        //only log in with substitute user if not already authenticated
+        //only challenger's name and avatar is stored for security
+        if(currentUser) {
+            const query = await getDoc(docRef)
+            // Just in case query.data() does not return anything, then send the user to the signin page.
+            if (query.data()) {
+              const info = (({name, avatar}) => ({name, avatar}))(query.data())
+              info.challengerID = code;
+              isShare === undefined && localStorage.setItem('challengerInfo', JSON.stringify(info))
+            } else {
+              history.push(`/signin`)
+            }
+        } else {
+            await auth.signInWithEmailAndPassword(
+                auth.getAuth(),
+                'wkvxgesiknosbamhei@kvhrr.com',
+                dummyPassword
+            );
+            const query = await getDoc(docRef)
+            const info = (({name, avatar}) => ({name, avatar}))(query.data())
+            info.challengerID = code;
+            localStorage.setItem('challengerInfo', JSON.stringify(info))
+            auth.getAuth().signOut()
+        }
+        setChallengerInfo(JSON.parse(localStorage.getItem("challengerInfo")))
     }
-    setChallengerInfo(JSON.parse(localStorage.getItem("challengerInfo")));
-  }
 
-  useEffect(() => {
-    code
-      ? getChallengerInfo()
-      : setChallengerInfo(JSON.parse(localStorage.getItem("challengerInfo")));
-  }, []);
+    // If code that is gotten from the url is playerwelcome or isShare is true then, if there is challengerInfo in
+    // local storage then set it to challengerInfo, else send the user to the signin page. For eveything else run getChallengerInfo.
+    useEffect(() => {
+        code === "playerwelcome" || isShare
+        ? localStorage.getItem("challengerInfo") ? setChallengerInfo(JSON.parse(localStorage.getItem("challengerInfo"))) : history.push(`/signin`)
+        : getChallengerInfo()
+    }, []);
 
-  useEffect(() => {
-    challengerInfo && setLoading(false);
-    console.log(currentUser);
-  }, [challengerInfo]);
+    // Render page after challengerInfo is gotten.
+    useEffect(() => {
+        challengerInfo && setLoading(false);
+    }, [challengerInfo])
 
-  return loading === false ? (
-    <div className="player-welcome">
-      <div className="top">
-        <h1 className="top-heading">
-          <h1 className="underline">Support</h1>{" "}
-          {challengerInfo && challengerInfo.name !== null
-            ? `${challengerInfo.name}'s`
-            : "the"}{" "}
-          8by8 Challenge!
-        </h1>
-        <div className="text">
-          <p>
-            <b>
-              Help{" "}
-              {challengerInfo && challengerInfo.name !== null
-                ? challengerInfo.name
-                : "your friend"}{" "}
-              win their <u>8by8 Challenge</u> by registering to vote or taking
-              other actions to #stopasianhate!
-            </b>
-          </p>
+    // If isShare is undefined, when playerwelcome page is rendered not in invite or share, then the buttons work.
+    return (
+        loading === false ? (
+        <div className="player-welcome">
+          <div className="top">
+            <h1 className="top-heading">
+              <u className="underline">Support</u> {challengerInfo && challengerInfo.name!==null ? `${challengerInfo.name}'s`: 'the'} 8by8 Challenge!
+            </h1>
+
+              <p className="text">
+                <b>
+                  Help {challengerInfo && challengerInfo.name!==null ? challengerInfo.name: 'your friend'} win their <u>8BY8 Challenge</u> by registering to vote or taking other actions to #stopasianhate!
+                </b>
+              </p>
+
+            <Button className="getStarted-button" onClick={() => {isShare === undefined && history.push(`/actions`)}}>Get Started</Button>
+            <div align="center">
+              <p className="small-text">
+                Already have an account? <button className="signin-link blue" onClick={() => {isShare === undefined && history.push(`/signin`)}}>Sign In</button>
+              </p>
+            </div>
+          </div>
+          <img src={BlackCurve} className="curve" alt="Black Curve"/>
+          <div className="main-content">
+            <div>
+              <h3 className="heading"><u className="underline">Here's how it works</u></h3>
+            </div>
+            <p className="subheading">
+              1. Choose an action to take
+            </p>
+            <p className="text">
+              You can take any number of the available actions: register to vote, get election reminders or take the 8by8 challenge yourself. Pick one to start.
+            </p>
+            <div className="image">
+              <img src={SignUp1} alt="8by8 Logo" />
+            </div>
+            <p className="subheading">
+              2. Your friend will earn a badge
+            </p>
+            <p className="text">
+              Any of the 3 actions will help your friend earn a badge and get closer to winning the challenge.
+            </p>
+            <div className="image">
+              <img src={SignUp2} alt="8by8 Logo" />
+            </div>
+            <p className="subheading">
+              3. Come back and take more actions
+            </p>
+            <p className="text">
+              Whether it is to help the same friend or a different one, the more actions you take, the better!
+              Note that you can only help earn one badge per friend.
+            </p>
+            <div className="image">
+              <img src={Vote} alt="8by8 Logo" />
+            </div>
+            <Button className="getStarted-button" onClick={() => {isShare === undefined && history.push(`/actions`)}}>Get Started</Button>
+            <p align="center" className="small-text">
+              Already have an account? <button className="signin-link black" onClick={() => {isShare === undefined && history.push(`/signin`)}}>Sign In</button>
+            </p>
+          </div>
         </div>
-        <Button
-          onClick={() => {
-            history.push(`/actions`);
-          }}
-        >
-          Get Started
-        </Button>
-        <div align="center">
-          <p className="small-text">
-            Already have an account? <a href="/signin">Sign In</a>
-          </p>
-        </div>
-      </div>
-      <img src={BlackCurve} className="curve" />
-      <div className="main-content">
-        <div>
-          <h3 className="heading">
-            <u className="underline">Here's how it works</u>
-          </h3>
-        </div>
-        <p className="subheading">1. Choose an action to take</p>
-        <p className="text">
-          You can take any number of the available actions: register to vote,
-          get election reminders or take the 8by8 challenge yourself. Pick one
-          to start.
-        </p>
-        <div className="image">
-          <img src={SignUp1} alt="8by8 Logo" />
-        </div>
-        <p className="subheading">2. Your friend will earn a badge</p>
-        <p className="text">
-          Any of the 3 actions will help your friend earn a badge and get closer
-          to winning the challenge.
-        </p>
-        <div className="image">
-          <img src={SignUp2} alt="8by8 Logo" />
-        </div>
-        <p className="subheading">3. Come back and take more actions</p>
-        <p className="text">
-          Whether it is to help the same friend or a different one, the more
-          actions you take, the better! Note that you can only help earn one
-          badge per friend.
-        </p>
-        <div className="image">
-          <img src={Vote} alt="8by8 Logo" />
-        </div>
-        <Button
-          onClick={() => {
-            history.push(`/actions`);
-          }}
-        >
-          Get Started
-        </Button>
-        <p align="center" className="small-text">
-          Already have an account?{" "}
-          <a href="/signin">
-            <b>Sign In</b>
-          </a>
-        </p>
-      </div>
-    </div>
-  ) : (
-    <LoadingWheel overlay={false} />
-  );
+        ) : (
+        <LoadingWheel overlay={false}/>
+        )
+    )
 }
