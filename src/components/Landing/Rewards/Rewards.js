5f637a7e6ae7f9c7e9a936bb6a4f554e4b19b51f
Arnab Ghosh <arnabcare21@gmail.com>
2022-08-03 21:34:09 -0600

    Add placeholder images, final styling

--- src/components/Landing/Rewards/Rewards.js
+++ src/components/Landing/Rewards/Rewards.js
@@ -1,13 +1,18 @@
 import CurveA from "./../../../assets/2-shapes/curve-a.svg";
 import './Rewards.scss';
+import { useHistory } from "react-router-dom";
+import placeholderImage from "./../../../assets/images/placeholder-image.jpg"
 
 const Rewards = () => {
+    const history = useHistory();
+
     // Consistent data type
     // Also includes key since we will be iterating over it
     class Partner {
         static key = 0;
-        constructor(name, img, type, location) {
+        constructor(name, link, img, type, location) {
             this.name = name;
+            this.link = link;
             this.img = img;
             this.type = type;
             this.location = location;
@@ -15,17 +20,19 @@ const Rewards = () => {
         }
     }
 
-    
+
     // Sample Data
-    const samplePartner = () => new Partner (
+    const samplePartner = () => new Partner(
         "Joe's Sandwhiches",
-        "https://c8.alamy.com/comp/FC20MT/sandwich-shop-in-milan-FC20MT.jpg",
+        "#",
+        "",
         "Restaurant",
         "San Jose, CA"
     )
 
     // Store the list of partners here
     // Generate them with the constructor
+    // That way there is a key
     const partners = [
         samplePartner(),
         samplePartner(),
@@ -46,15 +53,34 @@ const Rewards = () => {
             </div>
             <img className="curve" src={CurveA} alt="black curve" />
             <div className="sponsor-section">
+                { /* If we have partners */}
                 {partners.length > 0 && partners.map((partner) => {
                     return (
                         <div className="sponsor" key={partner.id}>
-                            <img src={partner.img} alt={`${partner.name} image`} />
-                            <h5>{partner.name}</h5>
-                            <p>{partner.type} {partner.location}</p>
+                            <a href={partner.link}>
+                                <img src={partner.img === "" ? placeholderImage: partner.img} alt={`${partner.name} image`} />
+                                <h5>{partner.name}</h5>
+                                <p>{partner.type} {partner.location}</p>
+                            </a>
                         </div>
                     )
                 })}
+
+                { /* If we don't have any partners */}
+                {partners.length === 0 && (
+                    <div className="sponsor">
+                        <img src={placeholderImage} alt="Placeholder image" />
+                        <p>
+                            Unfortunately, we are not offering partner rewards at this time. You can still take the 8by8 Challenge to help the AAPI community!
+                        </p>
+                        <button
+                            className="challenge-button"
+                            onClick={() => history.push("/challengerwelcome")}
+                        >
+                            Take the Challenge!
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     )
