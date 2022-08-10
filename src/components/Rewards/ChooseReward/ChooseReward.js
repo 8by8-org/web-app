d0f7e07b2bec57605742f2a9cd16bf096ecab351
Arnab Ghosh <arnabcare21@gmail.com>
2022-08-09 19:50:07 -0600

    Update ChooseReward so that data is retrieved from the database

--- src/components/Rewards/ChooseReward/ChooseReward.js
+++ src/components/Rewards/ChooseReward/ChooseReward.js
@@ -3,6 +3,8 @@ import {useHistory} from "react-router-dom";
 import './ChooseReward.scss';
 import {useEffect, useState} from "react";
 import placeholderImage from "../../../assets/images/placeholder-image.jpg"
+import {database} from '../../../firebase';
+import {ref, child, get} from 'firebase/database';
 
 const ChooseReward = () => {
     // Get the current player
@@ -11,46 +13,34 @@ const ChooseReward = () => {
     /*
      Sample data, once again we need to make this empty once we get the partners in the database
      */
-    const [partners, setPartners] = useState([
-        {
-            inStore: true,
-            online: false,
-            slug: 'bobsburgers',
-            name: "Bob's Burgers",
-            description: "50% any entree w/ a large drink"
-        },
-        {
-            inStore: false,
-            online: true,
-            slug: 'janesjelly',
-            name: "Jane's Jelly Store",
-            description: "10% off any online order"
-        }
-    ]);
+    const [partners, setPartners] = useState([]);
 
     useEffect(() => {
+        // Get the partner data from the database
         const getDataFromDB = async () => {
-            // TODO: Implement this function
+            const dbRef = ref(database);
+            get(child(dbRef, `partners`)).then((snapshot) => {
+                const data = snapshot.val();
+                setPartners(Object.values(data));
+            });
         }
 
-        // Uncomment this line once the above is implemented
-        // getDataFromDB().then(res => setPartners(res));
+        getDataFromDB().then();
     }, [])
 
     const [selected, setSelected] = useState(null);
 
     // Calculate if the user is eligible for the challenge
     const isEligible = (user) => {
-        return true;
-        // Uncomment these before committing
-        // if(!user.badges) return false;
-        // return user.completedActionForChallenger || user.badges.length >= 8;
+        if (!user.badges) return false;
+        return user.completedActionForChallenger || user.badges.length >= 8;
     }
 
     if (!isEligible(currentUser)) {
         history.push('/progress');
     }
 
+    // Handle Form Submission
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(selected);
@@ -62,21 +52,23 @@ const ChooseReward = () => {
             <p className='about-text'>You've won the 8by8 Challenge! Now choose your reward, you've earned it!</p>
             <form>
                 <h2>In Store</h2>
-                {partners.filter(partner => partner.inStore).map((partner) => {
+                { /* We have some code duplication here - splitting into a component is more complicated than its worth */}
+                {partners.filter(partner => partner.rewardType === 'In-Store').map((partner) => {
                     return (
-                        <div key={partner.slug} className='my-3'>
-                            <input type="radio" name="sample" onChange={() => setSelected(partner.slug)} id={partner.slug}/>
-                            <label className="radio-label" htmlFor={partner.slug}>
+                        <div key={partner.name} className='my-3'>
+                            <input type="radio" name="sample" onChange={() => setSelected(partner.name)}
+                                   id={partner.name}/>
+                            <label className="radio-label" htmlFor={partner.name}>
                                 <div className="row partner-container">
                                     <div className="circle-container col-3">
                                         <div className="circle"></div>
                                     </div>
                                     <div className="partner-content col-6">
                                         <h3>{partner.name}</h3>
-                                        <p> {partner.description}</p>
+                                        <p> {partner.businessDescription}</p>
                                     </div>
                                     <div className="image-container col-3">
-                                        <img src={partner.img ? partner.img : placeholderImage} alt={'Partner image'}/>
+                                        <img src={ /* partner.logo ? partner.logo : placeholderImage */ placeholderImage} alt={'Partner image'}/>
                                     </div>
                                 </div>
                             </label>
@@ -84,21 +76,22 @@ const ChooseReward = () => {
                     );
                 })}
                 <h2>Online</h2>
-                {partners.filter(partner => partner.online).map((partner) => {
+                {partners.filter(partner => partner.rewardType === 'Online').map((partner) => {
                     return (
-                        <div key={partner.slug} className='my-3'>
-                            <input type="radio" name="sample" onChange={() => setSelected(partner.slug)} id={partner.slug}/>
-                            <label className="radio-label" htmlFor={partner.slug}>
+                        <div key={partner.name} className='my-3'>
+                            <input type="radio" name="sample" onChange={() => setSelected(partner.name)}
+                                   id={partner.name}/>
+                            <label className="radio-label" htmlFor={partner.name}>
                                 <div className="row partner-container">
                                     <div className="circle-container col-3">
                                         <div className="circle"></div>
                                     </div>
                                     <div className="partner-content col-6">
                                         <h3>{partner.name}</h3>
-                                        <p> {partner.description}</p>
+                                        <p> {partner.businessDescription}</p>
                                     </div>
                                     <div className="image-container col-3">
-                                        <img src={partner.img ? partner.img : placeholderImage} alt={'Partner image'}/>
+                                        <img src={ /* partner.logo ? partner.logo : placeholderImage */ placeholderImage} alt={'Partner image'}/>
                                     </div>
                                 </div>
                             </label>
