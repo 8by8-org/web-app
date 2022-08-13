8f381586cff27c80eee19fe6749c0347b3104564
zjiao19 <zjiao19@outlook.com>
2022-08-13 12:42:05 -0400

    save voter reg info to firebase

---
+++ src/components/VoterRegistration/utils/UpdateRegInfo.js
@@ -0,0 +1,18 @@
+import { auth } from "../../../firebase";
+import { getFirestore, doc, updateDoc } from "firebase/firestore";
+const db = getFirestore();
+
+export async function addRegInfoToDB(infoBody) {
+  const user = auth.getAuth().currentUser;
+  updateDoc(doc(db, "users", user.uid), {
+    voteInfo: infoBody
+  });
+}
+
+export async function clearVoterRegInfo() {
+  const user = auth.getAuth().currentUser;
+  updateDoc(doc(db, "users", user.uid), {
+    isRegisteredVoter: false,
+    voteInfo: {}
+  });
+}
