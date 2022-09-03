0b2d4642c3b7a2e33a70c1bc6486d4b88fb3b04c
Phillip D <89946282+CaptainExtremis@users.noreply.github.com>
2022-09-03 13:26:09 -0400

    Add files via upload

--- src/components/Landing/PlayerWelcome/PlayerWelcome.js
+++ src/components/Landing/PlayerWelcome/PlayerWelcome.js
@@ -3,14 +3,13 @@ import { useHistory } from "react-router-dom";
 import { getFirestore, getDoc, doc } from "firebase/firestore";
 import SignUp1 from "../../../assets/images/PlayerWelcome/SignUp1.png";
 import SignUp2 from "../../../assets/images/PlayerWelcome/SignUp2.png";
-import Vote from "../../../assets/images/PlayerWelcome/Vote1.png";
+import PlayerWelcome from "../../../assets/images/PlayerWelcome/Playerwelcome3.png";
 import BlackCurve from "../../../assets/images/PlayerWelcome/BlackCurve.svg";
 import "./PlayerWelcome.scss";
 import { auth } from "../../../firebase";
 import { dummyPassword } from "../../../constants";
 import { useAuth } from "../../../contexts/AuthContext";
 import { LoadingWheel } from "./../../Utility/LoadingWheel/LoadingWheel.component";
-import StepFour from "../../../assets/images/ChallengerWelcome/StepFour.png";
 
 export default function PlayerWelcome({ isShare }) {
   const history = useHistory();
@@ -62,19 +61,12 @@ export default function PlayerWelcome({ isShare }) {
   // If code that is gotten from the url is playerwelcome or isShare is true then, if there is challengerInfo in
   // local storage then set it to challengerInfo, else send the user to the signin page. For eveything else run getChallengerInfo.
   useEffect(() => {
-    if (currentUser?.uid === code) {
-      history.push(`/signin`);
-    } else {
-      code === "playerwelcome" || isShare
-        ? localStorage.getItem("challengerInfo")
-          ? setChallengerInfo(
-              JSON.parse(localStorage.getItem("challengerInfo"))
-            )
-          : history.push(`/signin`)
-        : getChallengerInfo();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [history, code, isShare]);
+    code === "playerwelcome" || isShare
+      ? localStorage.getItem("challengerInfo")
+        ? setChallengerInfo(JSON.parse(localStorage.getItem("challengerInfo")))
+        : history.push(`/signin`)
+      : getChallengerInfo();
+  }, []);
 
   // Render page after challengerInfo is gotten.
   useEffect(() => {
@@ -83,12 +75,8 @@ export default function PlayerWelcome({ isShare }) {
 
   // If isShare is undefined, when playerwelcome page is rendered not in invite or share, then the buttons work.
   return loading === false ? (
-    <div
-      className={
-        "player-welcome" + (isShare || isShare === false ? " preview" : "")
-      }
-    >
-      <div className="top" style={{ textAlign: "center" }}>
+    <div className={"player-welcome" + ((isShare || isShare === false) ? " preview" : "")}>
+      <div className="top">
         <h1 className="top-heading">
           <span className="underline h1">Support</span>{" "}
           {challengerInfo && challengerInfo.name !== null
@@ -131,15 +119,6 @@ export default function PlayerWelcome({ isShare }) {
             </p>
           </div>
         )}
-        <p
-          onClick={() => {
-            history.push(`/why8by8`);
-          }}
-          className="link"
-          style={{ color: "#02DDC3", zIndex: "1", marginTop: "0.7em" }}
-        >
-          See why others are doing it
-        </p>
       </div>
       <img src={BlackCurve} className="curve" alt="Black Curve" />
       <div className="main-content">
@@ -168,18 +147,8 @@ export default function PlayerWelcome({ isShare }) {
           badge per friend.
         </p>
         <div className="image">
-          <img src={Vote} alt="8by8 Logo" />
+          <img src={PlayerWelcome} alt="8by8 Logo" />
         </div>
-        <h3>4. Get a Reward!</h3>
-        <p className="text">
-          When your friend wins the challenge within 8 days, you win as well!
-          Then select and enjoy a reward from one of our amazing partners.
-        </p>
-        <img
-          src={StepFour}
-          alt="earn 8 badges in 8 days"
-          className="center-img"
-        />
         <button
           className="gradient"
           onClick={() => {
