7380be04b367da689619247c96b70332ca26c646
Joseph Dvorak <dvorakjt@gmail.com>
2023-06-15 02:34:01 -0400

    updated player links to link to privacy policy

--- src/components/header/hamburger-menu/links/player-links.tsx
+++ src/components/header/hamburger-menu/links/player-links.tsx
@@ -8,13 +8,13 @@ export default function PlayerLinks() {
       <HamburgerLink href="/challengerwelcome" className={styles.take_the_challenge_btn}>
         Take The Challenge
       </HamburgerLink>
-      <HamburgerLink href={"/actions"} className={styles.link_lg_top}>
+      <HamburgerLink href="/actions" className={styles.link_lg_top}>
         Take Action
       </HamburgerLink>
-      <HamburgerLink href={"/why8by8"} className={styles.link_lg}>Why 8by8</HamburgerLink>
-      <HamburgerLink href={"/rewards"} className={styles.link_lg}>Rewards</HamburgerLink>
-      <HamburgerLink href={"/faq"} className={styles.link_lg}>FAQS</HamburgerLink>
-      <HamburgerLink href="/privacy" className={styles.link_sm_top}>Privacy Policy</HamburgerLink>
+      <HamburgerLink href="/why8by8" className={styles.link_lg}>Why 8by8</HamburgerLink>
+      <HamburgerLink href="/rewards" className={styles.link_lg}>Rewards</HamburgerLink>
+      <HamburgerLink href="/faq" className={styles.link_lg}>FAQS</HamburgerLink>
+      <HamburgerLink href="/privacy-policy" className={styles.link_sm_top}>Privacy Policy</HamburgerLink>
       <HamburgerLink href="/settings" className={styles.link_sm}>Settings</HamburgerLink>
       <SignoutBtn />
     </>
