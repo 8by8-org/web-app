5150271244784259f8de60410592cdf98e4b3f19
Joseph Dvorak <dvorakjt@gmail.com>
2023-06-15 02:28:53 -0400

    updated header links to link to privacy-policy

--- src/components/header/hamburger-menu/links/challenger-links.tsx
+++ src/components/header/hamburger-menu/links/challenger-links.tsx
@@ -11,10 +11,10 @@ export default function ChallengerLinks() {
       >
         My Challenge
       </HamburgerLink>
-      <HamburgerLink href={"/why8by8"} className={styles.link_lg}>Why 8by8</HamburgerLink>
-      <HamburgerLink href={"/rewards"} className={styles.link_lg}>Rewards</HamburgerLink>
-      <HamburgerLink href={"/faq"} className={styles.link_lg}>FAQS</HamburgerLink>
-      <HamburgerLink href="/privacy" className={styles.link_sm_top}>Privacy Policy</HamburgerLink>
+      <HamburgerLink href="/why8by8" className={styles.link_lg}>Why 8by8</HamburgerLink>
+      <HamburgerLink href="/rewards" className={styles.link_lg}>Rewards</HamburgerLink>
+      <HamburgerLink href="/faq" className={styles.link_lg}>FAQS</HamburgerLink>
+      <HamburgerLink href="/privacy-policy" className={styles.link_sm_top}>Privacy Policy</HamburgerLink>
       <HamburgerLink href="/settings" className={styles.link_sm}>Settings</HamburgerLink>
       <SignoutBtn />
     </>
--- src/components/header/hamburger-menu/links/signed-out-links.tsx
+++ src/components/header/hamburger-menu/links/signed-out-links.tsx
@@ -9,7 +9,7 @@ export default function SignedOutLinks() {
       <HamburgerLink href="/why8by8" className={styles.link_lg}>Why 8by8</HamburgerLink>
       <HamburgerLink href="/rewards" className={styles.link_lg}>Rewards</HamburgerLink>
       <HamburgerLink href="/faq" className={styles.link_lg}>FAQS</HamburgerLink>
-      <HamburgerLink href="/privacy" className={styles.link_sm_top}>Privacy Policy</HamburgerLink>
+      <HamburgerLink href="/privacy-policy" className={styles.link_sm_top}>Privacy Policy</HamburgerLink>
       <HamburgerLink href="/settings" className={styles.link_sm}>Settings</HamburgerLink>
       <HamburgerLink href="/signup" className={styles.link_sm}>Sign up</HamburgerLink>
     </>
