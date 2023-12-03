02fea377d1fa88313ec5a64820690800bf09fbc8
Joseph Dvorak <dvorakjt@gmail.com>
2023-12-03 15:13:22 -0500

    #218a - Migrate Privacy Policy to Next.js (#223)
    * set up next.js environment and add workflows
    
    * testing that branch protection rule works when jest tests fail
    
    * updated ci test script to use -u flag
    
    * testing that failed tests prevent pushing
    
    * retesting branch protection rule
    
    * testing that lack of code coverage causes push to fail
    
    * testing that successful tests allow the push
    
    * testing failing tests
    
    * all tests should now pass
    
    * added .env* to gitignore
    
    * added header, user model, and user context
    
    * split links into separate components for each user type and activeUser state
    
    * updated header to leverage react context api
    
    * created header context, completed header, hamburger menu, signout modal and popup modal
    
    * added unit tests for header and related components
    
    * add footer, minor styling updates to page container, home page
    
    * ported privacy policy
    
    * updated header links to link to privacy-policy
    
    * updated player links to link to privacy policy

--- src/components/header/hamburger-menu/links/hybrid-links.tsx
+++ src/components/header/hamburger-menu/links/hybrid-links.tsx
@@ -5,13 +5,13 @@ import styles from '@/styles/modules/components/header/hamburger-menu/links/hybr
 export default function HybridLinks() {
   return (
     <>
-      <HamburgerLink href={"/progress"} className={styles.link_lg_top}>My Challenge</HamburgerLink>
-      <HamburgerLink href={"/actions"} className={styles.link_lg}>Take Action</HamburgerLink>
-      <HamburgerLink href={"/share"} className={styles.link_lg}>Share</HamburgerLink>
-      <HamburgerLink href={"/why8by8"} className={styles.link_lg}>Why 8by8</HamburgerLink>
-      <HamburgerLink href={"/rewards"} className={styles.link_lg}>Rewards</HamburgerLink>
-      <HamburgerLink href={"/faq"} className={styles.link_lg}>FAQS</HamburgerLink>
-      <HamburgerLink href="/privacy" className={styles.link_sm_top}>Privacy Policy</HamburgerLink>
+      <HamburgerLink href="/progress" className={styles.link_lg_top}>My Challenge</HamburgerLink>
+      <HamburgerLink href="/actions" className={styles.link_lg}>Take Action</HamburgerLink>
+      <HamburgerLink href="/share" className={styles.link_lg}>Share</HamburgerLink>
+      <HamburgerLink href="/why8by8" className={styles.link_lg}>Why 8by8</HamburgerLink>
+      <HamburgerLink href="/rewards" className={styles.link_lg}>Rewards</HamburgerLink>
+      <HamburgerLink href="/faq" className={styles.link_lg}>FAQS</HamburgerLink>
+      <HamburgerLink href="/privacy-policy" className={styles.link_sm_top}>Privacy Policy</HamburgerLink>
       <HamburgerLink href="/settings" className={styles.link_sm}>Settings</HamburgerLink>
       <SignoutBtn />
     </>
